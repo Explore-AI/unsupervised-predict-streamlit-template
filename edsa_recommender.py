25a503617bf1c54e11c90867b26d32be918f0606
Bryan Green <bryangreen290@gmail.com>
2020-07-13 11:00:40 +0200

    changed the background and text color

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -40,6 +40,20 @@ from recommenders.content_based import content_model
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
 
+# Change background colours and text format
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+
+#def icon(icon_name):
+    #st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
 # App declaration
 def main():
 
