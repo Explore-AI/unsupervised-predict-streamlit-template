cfbef52d18c41b63756750e1f886d52548e64774
Tumisang-hub <60347402+Tumisang-hub@users.noreply.github.com>
2020-07-20 18:13:30 +0200

    Add files via upload

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,12 +45,15 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Home","Recommender System","Solution Overview","Our Team"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
+    if page_selection == "Home":
+        st.markdown("# Welcome")
+
     if page_selection == "Recommender System":
         # Header contents
         st.write('# Movie Recommender Engine')
@@ -101,12 +104,132 @@ def main():
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Solution Overview":
-        st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
+        cont="""
+         <!-- header of website layout -->
+        <div style="background-color: #fffd80;padding: 15px;text-align: center;width: 100%"> 
+            <h2 style = "color:black;font-size:200%"> 
+                <b>Solution Overview</b> 
+            </h2> 
+        </div> 
+        <!-- nevigation menu of website layout -->
+        <div style="overflow: hidden;background-color: #333;width: 100%"> 
+            <a href = "#" style="float: left;display: block;color: white;text-align: center;padding: 14px 16px;text-decoration: none;">Home</a> 
+            <a href = "#" style="float: left;display: block;color: white;text-align: center;padding: 14px 16px;text-decoration: none;">EDA</a> 
+            <a href = "#" style="float: left;display: block;color: white;text-align: center;padding: 14px 16px;text-decoration: none;">Our Team</a> 
+        </div> 
+        <!-- Content section of website layout -->
+        <div class = "row"> 
+            <div style="float: left;width: 34%;padding: 15px;text-align:justify;background-color: #f0f2f6;"> 
+                <h2 style="color:#f63366;text-align:center;">Column A</h2> 
+                <p>Prepare for the Recruitment drive of product 
+                based companies like Microsoft, Amazon, Adobe 
+                etc with a free online placement preparation 
+                course. The course focuses on various MCQ's 
+                & Coding question likely to be asked in the 
+                interviews & make your upcoming placement 
+                season efficient and successful.</p> 
+            </div>
+            <div style="float: left;width: 33%;padding: 15px;text-align:justify;background-color: #f0f2f6;"> 
+                <h2 style="color:#f63366;text-align:center;">Column A</h2> 
+                <p>Prepare for the Recruitment drive of product 
+                based companies like Microsoft, Amazon, Adobe 
+                etc with a free online placement preparation 
+                course. The course focuses on various MCQ's 
+                & Coding question likely to be asked in the 
+                interviews & make your upcoming placement 
+                season efficient and successful.</p> 
+            </div>
+            <div style="float: left;width: 33%;padding: 15px;text-align:justify;background-color: #f0f2f6;"> 
+                <h2 style="color:#f63366;text-align:center;">Column A</h2> 
+                <p>Prepare for the Recruitment drive of product 
+                based companies like Microsoft, Amazon, Adobe 
+                etc with a free online placement preparation 
+                course. The course focuses on various MCQ's 
+                & Coding question likely to be asked in the 
+                interviews & make your upcoming placement 
+                season efficient and successful.</p> 
+            </div>
+        </div> 
+        """
+        st.markdown(cont, unsafe_allow_html=True)
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
+    if page_selection == "Our Team":
+        team="""
 
+	<div style="background: #f1f2f6;text-align: center;">
+		<div style="max-width: 1500px;margin: auto;padding: 40px;color: #333;overflow: hidden;">
+      <h2><b>Meet our Team</b></h2>
+      <!---Team members---->
+				<div style="float: left;width: calc(100% / 3);overflow: hidden;padding: 40px 0;transition: 0.4s;">
+					<img style="width: 150px;height: 150px;border-radius: 50%;" src="https://github.com/Tumisang-hub/Unsupervised-Sprint/blob/master/RNT03793-2%20(2).JPG?raw=true" alt="rafeh">
+					<div style="margin: 5px;text-transform: uppercase;">Rohini Jagath</div>
+					<div style="font-style: italic;color: #3498db;">Data Scientist</div>
+					<div style=";color: #f63366;"><p>Passionate about all things data... but she won't share popcorn at the movies.</p></div>
+					<div style="margin-top: 6px;">
+						<a href="#" style="margin: 0 4px;display: inline-block;width: 30px;height: 30px;transition: 0.4s;"><img border="0" alt="Linkein" src="https://image.flaticon.com/icons/svg/1384/1384014.svg" width="25" height="25"></a>
+					</div>
+				</div>
+<!-------------------->
+<!---Team members---->
+				<div style="float: left;width: calc(100% / 3);overflow: hidden;padding: 40px 0;transition: 0.4s;">
+					<img style="width: 150px;height: 150px;border-radius: 50%;" src="https://github.com/Tumisang-hub/Unsupervised-Sprint/blob/master/pila.jpg?raw=true">
+					<div style="margin: 5px;text-transform: uppercase;">Pilasande Pakkies</div>
+					<div style="font-style: italic;color: #3498db;">Data Scientist</div>
+					<div style=";color: #f63366;"><p>An enthusiast who loves volleyball more than any sport. A pizza lover.</p></div>
+					<div style="margin-top: 6px;">
+						<a href="http://www.linkedin.com/in/Pilasande-Pakkies" style="margin: 0 4px;display: inline-block;width: 30px;height: 30px;transition: 0.4s;"><img border="0" alt="Linkein" src="https://image.flaticon.com/icons/svg/1384/1384014.svg" width="25" height="25"></a>
+					</div>
+				</div>
+<!-------------------->
+<!---Team members---->
+				<div style="float: left;width: calc(100% / 3);overflow: hidden;padding: 40px 0;transition: 0.4s;">
+					<img style="width: 150px;height: 150px;border-radius: 50%;" src="https://github.com/Tumisang-hub/Unsupervised-Sprint/blob/master/20191130_203409.jpg?raw=true" alt="rafeh">
+					<div style="margin: 5px;text-transform: uppercase;">Tumisang Sentle</div>
+					<div style="font-style: italic;color: #3498db;">Web Developer</div>
+					<div style=";color: #f63366;"><p>A math addict. He loves listening to debates but he can't debate.</p></div>
+					<div style="margin-top: 6px;">
+						<a href="https://www.linkedin.com/in/tumisang-sentle-53100a1a5/" style="margin: 0 4px;display: inline-block;width: 30px;height: 30px;transition: 0.4s;"><img border="0" alt="Linkein" src="https://image.flaticon.com/icons/svg/1384/1384014.svg" width="25" height="25"></a>
+					</div>
+				</div>
+<!-------------------->
+<!---Team members---->
+				<div style="float: left;width: calc(100% / 3);overflow: hidden;padding: 40px 0;transition: 0.4s;">
+					<img style="width: 150px;height: 150px;border-radius: 50%;" src="https://github.com/Tumisang-hub/Unsupervised-Sprint/blob/master/60532fb212cb493f8fc0c629ef61aa1b.jpg?raw=true" alt="rafeh">
+					<div style="margin: 5px;text-transform: uppercase;">Refentse Motlogelwa</div>
+					<div style="font-style: italic;color: #3498db;">Data Engineer</div>
+					<div style=";color: #f63366;"><p>A lover of life with good statistics background. He is also a great DJ.</p></div>
+					<div style="margin-top: 6px;">
+						<a href="https://www.linkedin.com/in/ramotse-motlogelwa-8a09358b" style="margin: 0 4px;display: inline-block;width: 30px;height: 30px;transition: 0.4s;"><img border="0" alt="Linkein" src="https://image.flaticon.com/icons/svg/1384/1384014.svg" width="25" height="25"></a>
+					</div>
+				</div>
+<!-------------------->
+<!---Team members---->
+				<div style="float: left;width: calc(100% / 3);overflow: hidden;padding: 40px 0;transition: 0.4s;">
+					<img style="width: 150px;height: 150px;border-radius: 50%;" src="https://media-exp1.licdn.com/dms/image/C5603AQEtkA_hyaeciA/profile-displayphoto-shrink_800_800/0?e=1600300800&v=beta&t=i2wG9MJ8LyVMEYkssSfzhKKIoCCmruWTIlt92QEFT9U">
+					<div style="margin: 5px;text-transform: uppercase;">Philani Mkhize</div>
+					<div style="font-style: italic;color: #3498db;">Data Analyst</div>
+					<div style=";color: #f63366;"><p>A problem is easier when broken into smaller easier problem.</p></div>
+					<div style="margin-top: 6px;">
+						<a href="https://www.linkedin.com/in/philani-mkhize-519995149/" style="margin: 0 4px;display: inline-block;width: 30px;height: 30px;transition: 0.4s;"><img border="0" alt="Linkein" src="https://image.flaticon.com/icons/svg/1384/1384014.svg" width="25" height="25"></a>
+					</div>
+				</div>
+<!-------------------->
+<!---Team members---->
+				<div style="float: left;width: calc(100% / 3);overflow: hidden;padding: 40px 0;transition: 0.4s;">
+					<img style="width: 150px;height: 150px;border-radius: 50%;" src="https://github.com/Tumisang-hub/Unsupervised-Sprint/blob/master/IMG-2211.jpg?raw=true" alt="rafeh">
+					<div style="margin: 5px;text-transform: uppercase;">Sandile Mkize</div>
+					<div style="font-style: italic;color: #3498db;">Data Analyst</div>
+					<div style=";color: #f63366;"><p>A former side stepper from the rugby field who turned into a data scientist.</p></div>
+					<div style="margin-top: 6px;">
+						<a href="https://www.linkedin.com/in/sandile-mkize-2395b4161/" style="margin: 0 4px;display: inline-block;width: 30px;height: 30px;transition: 0.4s;"><img border="0" alt="Linkein" src="https://image.flaticon.com/icons/svg/1384/1384014.svg" width="25" height="25"></a>
+					</div>
+				</div>
+<!-------------------->
+
+                """
+        st.markdown(team, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
