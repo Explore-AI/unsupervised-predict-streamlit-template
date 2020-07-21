954d04a73100f10c577d7925710c13d84df9fd2d
zee-ty <tyeko74@gmail.com>
2020-07-21 02:07:49 +0200

    testing content based app

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -32,7 +32,8 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import sigmoid_kernel
 
 # Importing data
 movies = pd.read_csv('resources/data/movies.csv', sep = ',',delimiter=',')
@@ -54,13 +55,26 @@ def data_preprocessing(subset_size):
 
     """
     # Split genre data into individual words.
-    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
+    movies['genres'] = movies['genres'].str.replace('|', ' ')
     # Subset of the data
     movies_subset = movies[:subset_size]
     return movies_subset
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
+movies_com = data_preprocessing(27000)
+movies_com = pd.merge(movies, ratings, on='movieId')
+movies_com['title'] = movies_com['genres'].astype(str)
+# Using TF-IDF
+tfv = TfidfVectorizer(min_df=3,  max_features=None, 
+            strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
+            ngram_range=(1, 3), use_idf=1,smooth_idf=1,sublinear_tf=1,
+            stop_words = 'english')
+
+# Fitting the TF-IDF on the 'overview' text
+tfv_matrix = tfv.fit_transform(movies_com['genres'])
+
+
 def content_model(movie_list,top_n=10):
     """Performs Content filtering based upon a list of movies supplied
        by the app user.
@@ -78,35 +92,28 @@ def content_model(movie_list,top_n=10):
         Titles of the top-n movie recommendations to the user.
 
     """
-    # Initializing the empty list of recommended movies
-    recommended_movies = []
-    data = data_preprocessing(27000)
-    # Instantiating and generating the count matrix
-    count_vec = CountVectorizer()
-    count_matrix = count_vec.fit_transform(data['keyWords'])
-    indices = pd.Series(data['title'])
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    # Getting the index of the movie that matches the title
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-    # Getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+    # Compute the sigmoid kernel
+    sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
 
-    # Store movie names
-    recommended_movies = []
-    # Appending the names of movies
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies['title'])[i])
+    # Reverse mapping of indices and movie titles
+    indices = pd.Series(movies.index, index=movies['title']).drop_duplicates()
+    # Get the index corresponding to original_title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores 
+    sig_scores = list(enumerate(sig[idx]))
+
+    # Sort the movies 
+    sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
+
+    # Scores of the 10 most similar movies
+    sig_scores = sig_scores[1:11]
+
+    # Movie indices
+    movie_indices = [i[0] for i in sig_scores]
+
+    # Top 10 most similar movies
+    recommended_movies =  movies['title'].iloc[movie_indices]
+
+
     return recommended_movies
