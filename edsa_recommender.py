ea6a408549e7f6a1aebf58d55790ed78e21931e7
bmsimang <bmsimang@student.wethinkcode.co.za>
2020-07-24 19:39:45 +0200

    content_filter

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -74,6 +74,20 @@ def main():
 
         # Perform top-10 movie recommendation generation
         
+        if sys == 'Content Based Filtering':
+            if st.button("Recommend"):
+                try:
+                    with st.spinner('Crunching the numbers...'):
+                        top_recommendations = content_model(movie_list=fav_movies,
+                                                            top_n=10)
+                    st.title("We think you'll like:")
+                    for i,j in enumerate(top_recommendations):
+                        st.subheader(str(i+1)+'. '+j)
+                except:
+                    st.error("Oops! Looks like this algorithm does't work.\
+                              We'll need to fix it!")
+
+
         if sys == 'Collaborative Based Filtering':
             if st.button("Recommend"):
                 try:
@@ -86,6 +100,7 @@ def main():
                 except:
                     st.error("Oops! Looks like this algorithm does't work.\
                               We'll need to fix it!")
+
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------------
