b717742aa436687c732cdd61609cab521e4fbb64
PythonMaester <60380304+PythonMaester@users.noreply.github.com>
2020-07-25 11:11:56 +0200

    added the shell script to automate the process

---
+++ automate.sh
@@ -0,0 +1,9 @@
+# the following commands are straight from EDSA, this file should be executed 5 to 10 minutes after mounting the s3 bucket
+echo "the script has started"
+
+cd ~/
+sudo apt-get install automake autotools-dev fuse g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config -y
+cd unsupervised-predict-streamlit-template/
+pip install -r "requirements.txt"
+streamlit run edsa_recommender.py
+
