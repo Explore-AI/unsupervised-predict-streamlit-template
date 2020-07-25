6075dedfd1f6cf00178bda264433867e21b8dc12
PythonMaester <60380304+PythonMaester@users.noreply.github.com>
2020-07-25 23:10:16 +0200

    modified background color

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -82,7 +82,12 @@ df = pd.merge(left=df,right=imdb, left_on='movieId', right_on='movieId')
 df['year'] = df['title'].str.extract('(\d\d\d\d)')
 df['budget'] = df['budget'].str.replace('$', '').str.replace(',','')
 
+# changing background colour
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+local_css('style.css')
 #@st.cache(persist=True)
 # App declaration
 def main():
