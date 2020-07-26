b4287bcf1e5ff970ed535967640da3b2fd64c46e
xmaluka <marciomaluka@ymail.com>
2020-07-26 18:50:37 +0200

    200MB pickle and update to original EDSA list

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -47,28 +47,28 @@ model = decompress_pickle('../pickled_files/full_compressed.pbz2')
 
 # Building the Model
 #model=pickle.load(open('../pickled_files/training_df_1.pkl', 'rb'))
-
-def prediction_item(item_id):
-    """Short summary.
-    Parameters
-    ----------
-    item_id : type
-        Description of parameter `item_id`.
-    Returns
-    -------
-    type
-        Description of returned object.
-    """
-    # data preprosessing
-    reader = Reader(rating_scale=(0, 5))
-    load_df = Dataset.load_from_df(ratings_df,reader)
-    a_train = load_df.build_full_trainset()
-
-
-    predictions = []
-    for ui in a_train.all_users():
-        predictions.append(model.predict(iid=item_id,uid=ui, verbose = False))
-    return predictions
+#
+#def prediction_item(item_id):
+#    """Short summary.
+#    Parameters
+#    ----------
+#    item_id : type
+#        Description of parameter `item_id`.
+#    Returns
+#    -------
+#    type
+#        Description of returned object.
+#    """
+#    # data preprosessing
+#    reader = Reader(rating_scale=(0, 5))
+#    load_df = Dataset.load_from_df(ratings_df,reader)
+#    a_train = load_df.build_full_trainset()
+#
+#
+#    predictions = []
+#    for ui in a_train.all_users():
+#        predictions.append(model.predict(iid=item_id,uid=ui, verbose = False))
+#    return predictions
 
 def pred_movies(movie_list):
     """Short summary.
@@ -106,33 +106,41 @@ def collab_model(movie_list,top_n):
     type
         Description of returned object.
     """
+    reader = Reader(rating_scale=(0, 5))
+    load_df = Dataset.load_from_df(ratings_df,reader)
+    a_train = load_df.build_full_trainset()
 
-    indices = pd.Series(movies_df['title'])
-    movie_ids = pred_movies(movie_list)
-    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
-    for i in movie_ids :
-        df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
-    # Getting the cosine similarity matrix
-    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
-    print(movie_list)
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-     # appending the names of movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
-    recommended_movies = []
-    # choose top 50
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n + 1]:
-        recommended_movies.append(list(movies_df['title'])[i])
+
+    predictions = []
+    for ui in a_train.all_users():
+        predictions.append(model.predict(iid=1,uid=ui, verbose = False))
+    recommended_movies = predictions
+#    indices = pd.Series(movies_df['title'])
+#    movie_ids = pred_movies(movie_list)
+#    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
+#    for i in movie_ids :
+#        df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
+#    # Getting the cosine similarity matrix
+#    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
+#    print(movie_list)
+#    idx_1 = indices[indices == movie_list[0]].index[0]
+#    idx_2 = indices[indices == movie_list[1]].index[0]
+#    idx_3 = indices[indices == movie_list[2]].index[0]
+#    # creating a Series with the similarity scores in descending order
+#    rank_1 = cosine_sim[idx_1]
+#    rank_2 = cosine_sim[idx_2]
+#    rank_3 = cosine_sim[idx_3]
+#    # calculating the scores
+#    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
+#    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
+#    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
+#     # appending the names of movies
+#    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+#    recommended_movies = []
+#    # choose top 50
+#    top_50_indexes = list(listings.iloc[1:50].index)
+#    # Removing chosen movies
+#    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
+#    for i in top_indexes[:top_n + 1]:
+#        recommended_movies.append(list(movies_df['title'])[i])
     return recommended_movies
