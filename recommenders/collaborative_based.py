a6f0f2eba81f8fd96057fc8ceccb03aa36d4bcf7
xmaluka <marciomaluka@ymail.com>
2020-07-27 05:36:52 +0200

    200MB pickle and update to original EDSA list

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -120,7 +120,7 @@ def collab_model(movie_list,top_n):
         if movieid in movies_df.movieId:
             count = len(all_users[all_users['movieId']==movieid])
             average = all_users[all_users['movieId']==movieid].rating.mean()
-            temp_1 = pd.DataFrame({'movieId':movieid,'popularity':count*average})
+            temp_1 = pd.DataFrame({'movieId':movieid,'popularity':count*average},index=[0])
             temp_df = pd.concat([temp_df,temp_1])
 #            a[movieid] = count*average
 #    sorted_a = sorted(a.items(), key=lambda x: x[1],reverse=True)[:10]
