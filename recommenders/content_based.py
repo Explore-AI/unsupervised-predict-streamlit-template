0329df4ab760900ed9b8dc3cb7c2aa4e3c6d2220
Faatimah <faatimahmansoor@gmail.com>
2020-07-28 08:54:09 +0200

    Update content_based.py
    edited content_based file with our function

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -32,6 +32,7 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
@@ -41,19 +42,16 @@ movies.dropna(inplace=True)
 
 def data_preprocessing(subset_size):
     """Prepare data for use within Content filtering algorithm.
-
     Parameters
     ----------
     subset_size : int
         Number of movies to use within the algorithm.
-
     Returns
     -------
     Pandas Dataframe
         Subset of movies selected for content-based filtering.
-
     """
-    # Split genre data into individual words.
+    #Initiate an empty list for the genres
     movies['keyWords'] = movies['genres'].str.replace('|', ' ')
     # Subset of the data
     movies_subset = movies[:subset_size]
@@ -78,35 +76,31 @@ def content_model(movie_list,top_n=10):
         Titles of the top-n movie recommendations to the user.
 
     """
-    # Initializing the empty list of recommended movies
-    recommended_movies = []
     data = data_preprocessing(27000)
-    # Instantiating and generating the count matrix
-    count_vec = CountVectorizer()
-    count_matrix = count_vec.fit_transform(data['keyWords'])
-    indices = pd.Series(data['title'])
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    # Getting the index of the movie that matches the title
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-    # Getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+    count = CountVectorizer(stop_words='english')
+    count_matrix = count.fit_transform(data['keyWords'])
+    cosine_sim = linear_kernel(count_matrix, count_matrix)
+    indices = pd.Series(data.index, index=data['title']).drop_duplicates()
+    # Initializing the empty list of recommended movies
+    idx0 = indices[movie_list[0]]
+    idx1 = indices[movie_list[1]]
+    idx2 = indices[movie_list[2]]
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores0 = list(enumerate(cosine_sim[idx0]))
+    sim_scores1 = list(enumerate(cosine_sim[idx1]))
+    sim_scores2 = list(enumerate(cosine_sim[idx2]))
 
-    # Store movie names
-    recommended_movies = []
-    # Appending the names of movies
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies['title'])[i])
-    return recommended_movies
+    # Sort the movies based on the similarity scores
+    sim_scores0 = sorted(sim_scores0, key=lambda x: x[1], reverse=True)
+    sim_scores1 = sorted(sim_scores1, key=lambda x: x[1], reverse=True)
+    sim_scores2 = sorted(sim_scores2, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores0 + sim_scores1 + sim_scores2
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:top_n+1]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return data['title'].iloc[movie_indices].tolist()
