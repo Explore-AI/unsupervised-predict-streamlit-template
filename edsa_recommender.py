17741b1ccc145cc7bbcbf868ed29408c50ca37a7
Marisa0612 <69723652+Marisa0612@users.noreply.github.com>
2021-06-17 20:32:55 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -107,11 +107,11 @@ def main():
     # or to provide your business pitch.
 	
     # Surprise Me
-    if page_selection == "Suprise me":
-        st.subheader('Here, a movie will be randomly selected for you')
-        if st.button('Suprise me'):
-           st.write(random.choice(title_list))
-           st.balloons()
+ if page_selection == "Suprise me":
+    st.subheader('Here, a movie will be randomly selected for you')
+    if st.button('Suprise me'):
+       st.write(random.choice(title_list))
+       st.balloons()
 
 if __name__ == '__main__':
     main()
