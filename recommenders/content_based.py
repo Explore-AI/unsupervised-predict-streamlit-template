d7cce71996fd459af0be5b7f0a003e453040fd0b
Kwanda Mazibuko <kwanda2426@gmail.com>
2022-01-31 22:10:40 +0200

    Update content_based.py

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -39,81 +39,9 @@ movies = pd.read_csv('resources/data/movies.csv', sep = ',')#,delimiter=',')
 ratings = pd.read_csv('resources/data/ratings.csv')
 movies.dropna(inplace=True)
 
-#def data_preprocessing(subset_size):
-#    """Prepare data for use within Content filtering algorithm.
 
-#  Parameters
- #   ----------
-#  subset_size : int
-#        Number of movies to use within the algorithm.
-
-#    Returns
- #   -------
-#    Pandas Dataframe
-#        Subset of movies selected for content-based filtering.
-
-#    """
-    # Split genre data into individual words.
- #   movies['keyWords'] = movies['genres'].str.replace('|', ' ')
-    # Subset of the data
- #   movies_subset = movies[:subset_size]
- #   return movies_subset
-
-# !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
-# You are, however, encouraged to change its content.  
-#def content_model(movie_list,top_n=10):
-#    """Performs Content filtering based upon a list of movies supplied
-#       by the app user.
-
-#    Parameters
-#    ----------
-#    movie_list : list (str)
-#        Favorite movies chosen by the app user.
-#    top_n : type
-#        Number of top recommendations to return to the user.
-
-#    Returns
-#    -------
-#    list (str)
-#        Titles of the top-n movie recommendations to the user.
-
-#    """
-    # Initializing the empty list of recommended movies
- #   recommended_movies = []
- #   data = data_preprocessing(27000)
-    # Instantiating and generating the count matrix
- #   count_vec = CountVectorizer()
- #   count_matrix = count_vec.fit_transform(data['keyWords'])
- #   indices = pd.Series(data['title'])
- #   cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    # Getting the index of the movie that matches the title
- #   idx_1 = indices[indices == movie_list[0]].index[0]
- #   idx_2 = indices[indices == movie_list[1]].index[0]
- #   idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
- #   rank_1 = cosine_sim[idx_1]
- #   rank_2 = cosine_sim[idx_2]
- #   rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
- #   score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
- #   score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
- #   score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-    # Getting the indexes of the 10 most similar movies
- #   listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
-
-    # Store movie names
- #   recommended_movies = []
-    # Appending the names of movies
- #   top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
- #   top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
- #   for i in top_indexes[:top_n]:
- #       recommended_movies.append(list(movies['title'])[i])
- #   return recommended_movies
-
-    
 def data_preprocessing(data):
-    """Prepare data for use within Content filtering algorithm.
+    """ Preprocess data for use within for content-based filtering algorithm.
     Parameters
     ----------
     subset_size : int
