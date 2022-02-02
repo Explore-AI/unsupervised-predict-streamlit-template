a2d1d7f514b01ca8f3a397931419f3c7c04015b1
Kwanda Mazibuko <kwanda2426@gmail.com>
2022-02-03 00:34:06 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -49,7 +49,7 @@ def main():
     # you are welcome to add more options to enrich your app.
     page_options = ["Recommender System", "Exploratory Data Analysis", "Solution Overview"]
 
-    st.sidebar.image("default.png", use_column_width=True)
+    #st.sidebar.image("default.png", use_column_width=True)
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
@@ -107,62 +107,17 @@ def main():
         st.title("Solution Overview")
 
         st.write("Collaborative filtering is the process of predicting the interests of a user by identifying preferences and information from many users. This is done by filtering data for information or patterns using techniques involving collaboration among multiple agents, data sources, etc. The underlying intuition behind collaborative filtering is that if user A and B have similar taste in a product, then A and B are likely to have similar taste in other products as well.")
-        st.image("model-based.png")
+        #st.image("model-based.png")
         st.write("Memory based approaches — also often referred to as neighbourhood collaborative filtering. Essentially, ratings of user-item combinations are predicted on the basis of their neighbourhoods. This can be further split into user based collaborative filtering and item based collaborative filtering. User based essentially means that likeminded users are going to yield strong and similar recommendations. Item based collaborative filtering recommends items based on the similarity between items calculated using user ratings of those items.")
         st.write("Model based approaches — are predictive models using machine learning. Features associated to the dataset are parameterized as inputs of the model to try to solve an optimization related problem. Model based approaches include using things like decision trees, rule based approaches, latent factor models etc.")
 
     if page_selection == "Exploratory Data Analysis":
-        title_eda = """
-	    <div style="background-color:#464e5f00;padding:10px;border-radius:10px;margin:10px;border-style:solid; border-color:#000000; padding: 1em;">
-	    <h1 style="color:black;text-align:center;">Exploratory Data Analysis</h1>
-        """
+        st.write('#')
+        
 
-        st.image('Exploratory-Analysis-Banner-940px.jpg',use_column_width=True)
-    
-        sys_eda = st.selectbox("Choose an EDA section suitable for you",
-        ('Ratings','Movies','Directors','Genres'))
-
-        if sys_eda == "Ratings":
-
-                op_ratings = st.radio("Choose an option under ratings",("Rating distribution","Percentage of users per rating"))
-
-                if op_ratings == "Percentage of users per rating":
-                    st.image('users-per-rating.png',use_column_width=True)
-                if op_ratings == "Rating distribution":
-                    st.image('rating-distribution.png',use_column_width=True)
-
-        if sys_eda == "Genres":
-            op_genre = st.radio("Choose an option under Genres",("Genre distribution","Word cloud of movie genres"))
-            if op_genre == "Genre distribution":
-                st.image('popular-genres.png')
-            if op_genre == "Word cloud of movie genres":
-                st.image('popular-genres2.png',use_column_width=True)  
-
-        if sys_eda == "Directors":            
-            op_director = st.radio("Choose an option under directors", ("Top 25 most rated directors","Top 25 directors with most number of movies","10 highest rated director with over 10000 ratings","10 worst rated directors with over 10000 ratings")) 
-            if op_director == "Top 25 most rated directors":
-                st.image('top_25_most_D1.png',use_column_width=True)
-            if op_director == "Top 25 directors with most number of movies":
-                st.image('Top_25_directors_D2.png',use_column_width=True)
-            if op_director == "10 highest rated director with over 10000 ratings":
-                st.image('10_highest_rated_D3.png',use_column_width=True)
-            if op_director == "10 worst rated directors with over 10000 ratings":
-                st.image('10_worst_directors_D4.png',use_column_width=True)
-                        
-        if sys_eda == "Movies":
-                op_movies = st.radio("Choose an option under movies",("Top 25 most rated movies of all time","25  most rated movies of the 21st century","Top 10 best and worst rated movies with over 10000 ratings","Total movies released per year"))
-                if op_movies == "Top 25 most rated movies of all time":
-                    st.image('25_most_1.png',use_column_width=True)
-                if op_movies == "25  most rated movies of the 21st century":
-                    st.image('25_most_2.png',use_column_width=True)
-                if op_movies == "Top 10 best and worst rated movies with over 10000 ratings":
-                    st.image('10_best_3.png',use_column_width=True)
-                    st.image('10_worst_4.png',use_column_width=True)
-                if op_movies == "Total movies released per year":
-                    st.image('Total_5.png',use_column_width=True)         
-
-    # You may want to add more sections here for aspects such as an EDA,
-    # or to provide your business pitch.
-
+    if page_selection == "Home":
+        st.title("Home")
+        st.write('#')
+        st.image('pictures/appname.png',use_column_width=True)
 if __name__ == '__main__':
     main()
