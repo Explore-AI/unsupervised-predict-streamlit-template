aaae5a0149ff4a4d0ac94fbae2940a7ade594586
Jack Kamire <jacmire@gmail.com>
2022-05-04 21:10:12 +0300

    removed notebooks

--- resources/models/train_colbased.py
+++ resources/models/train_colbased.py
@@ -14,31 +14,25 @@ import pandas as pd
 from surprise import SVD
 import surprise
 import pickle
-from surprise.model_selection import cross_validate
 
 # Importing datasets
+<<<<<<< HEAD
 ratings = pd.read_csv('/Users/jack/Documents/GitHub/unsupervised-predict-streamlit-template/resources/data/ratings.csv')
+=======
+ratings = pd.read_csv('ratings.csv')
+>>>>>>> parent of c46fbb5 (minor additions)
 ratings.drop('timestamp',axis=1,inplace=True)
 
 def svd_pp(save_path):
     # Check the range of the rating
     min_rat = ratings['rating'].min()
     max_rat = ratings['rating'].max()
-
     # Changing ratings to their standard form
     reader = surprise.Reader(rating_scale = (min_rat,max_rat))
-
     # Loading the data frame using surprice
-    print("Loading Data...")
     data_load = surprise.Dataset.load_from_df(ratings, reader)
-
     # Insatntiating surpricce
-    print("Initiating model...")
-    method = SVD()
-
-    print("Performing cross_validation...")
-    cross_validate(method, data_load, measures=['RMSE'], cv=3, verbose=True)
-
+    method = SVD(n_factors = 200 , lr_all = 0.005 , reg_all = 0.02 , n_epochs = 40 , init_std_dev = 0.05)
     # Loading a trainset into the model
     model = method.fit(data_load.build_full_trainset())
     print (f"Training completed. Saving model to: {save_path}")
