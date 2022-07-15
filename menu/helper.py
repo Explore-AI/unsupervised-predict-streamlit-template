61e7c7721e219b7e93c282aa0524968310e6700a
SoulR95 <verwey.r007@gmail.com>
2022-07-15 18:14:07 +0200

    Helper page added
    Added helper.py file
    Imported menu.helper
    Inside helper.py is the title and intro.
    Added first segment.

---
+++ menu/helper.py
@@ -0,0 +1,8 @@
+#Help page info will come here
+import streamlit as st
+
+def helppage():
+    st.title("Need Help?")
+    st.write("**Welcome** viewer, Not sure what to do or where to be? We have the support you need.")
+    st.title("Recommender System")
+    
