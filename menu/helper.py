8c164a6a2b5a34071912978813cdca5bac2ebd84
SoulR95 <verwey.r007@gmail.com>
2022-07-17 17:54:14 +0200

    Added the mp4
    Added the mp4.
    
    Writing the bullets below also for better understanding.

--- menu/helper.py
+++ menu/helper.py
@@ -5,4 +5,13 @@ def helppage():
     st.title("Need Help?")
     st.write("**Welcome** viewer, Not sure what to do or where to be? We have the support you need.")
     st.title("Recommender System")
+<<<<<<< Updated upstream
     st.image('./resources/imgs/Rtest1.gif')
+=======
+    Recommend = open('././resources/imgs/Recommend.mp4', 'rb')
+    R_play = Recommend.read()
+
+    st.video(R_play)
+    
+    
+>>>>>>> Stashed changes
