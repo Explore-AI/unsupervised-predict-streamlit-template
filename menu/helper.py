88516f0d2d7c4068a246c85ca8fe58c5067eb871
SoulR95 <verwey.r007@gmail.com>
2022-07-17 15:17:33 +0200

    Added Rtest1
    Added gif to helper might change it to a .mp4 for better quality.
    
    Also it will give the user the ability to rewatch certain moments.

--- menu/helper.py
+++ menu/helper.py
@@ -5,4 +5,4 @@ def helppage():
     st.title("Need Help?")
     st.write("**Welcome** viewer, Not sure what to do or where to be? We have the support you need.")
     st.title("Recommender System")
-    
+    st.image('./resources/imgs/Rtest1.gif')
