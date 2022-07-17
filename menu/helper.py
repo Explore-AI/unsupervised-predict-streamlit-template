404f0acb3901531658ec17613a0a77f9f991353e
SoulR95 <verwey.r007@gmail.com>
2022-07-17 23:19:01 +0200

    fix

--- menu/helper.py
+++ menu/helper.py
@@ -5,22 +5,8 @@ def helppage():
     st.title("Need Help?")
     st.write("**Welcome** viewer, Not sure what to do or where to be? We have the support you need.")
     st.title("Recommender System")
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    st.image('./resources/imgs/Rtest1.gif')
-=======
-    Recommend = open('././resources/imgs/Recommend.mp4', 'rb')
-    R_play = Recommend.read()
+    
+    r_vid = open('Recommend.mp4', 'rb')
+    r_play = r_vid.read()
 
-    st.video(R_play)
-    
-    
->>>>>>> Stashed changes
-=======
-    
-    Recommend = open('Recommend.mp4', 'rb')
-    r_read = Recommend.read()
-
-    st.video(r_read)
-    
->>>>>>> Stashed changes
+    st.video(r_play)
