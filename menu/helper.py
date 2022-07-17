d71d90479c1ac5c7823300e7ebd78a3399c6a78a
SoulR95 <verwey.r007@gmail.com>
2022-07-17 22:48:10 +0200

    Attempting to Fix
    Trying to fix conflicts

--- menu/helper.py
+++ menu/helper.py
@@ -6,6 +6,7 @@ def helppage():
     st.write("**Welcome** viewer, Not sure what to do or where to be? We have the support you need.")
     st.title("Recommender System")
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     st.image('./resources/imgs/Rtest1.gif')
 =======
     Recommend = open('././resources/imgs/Recommend.mp4', 'rb')
@@ -13,5 +14,13 @@ def helppage():
 
     st.video(R_play)
     
+    
+>>>>>>> Stashed changes
+=======
+    
+    Recommend = open('Recommend.mp4', 'rb')
+    r_read = Recommend.read()
+
+    st.video(r_read)
     
 >>>>>>> Stashed changes
