b1af2129a906fe3b2a9dd176decb31cd042ba785
Emmanuel Tamara David <63945679+emmzytamara@users.noreply.github.com>
2022-07-20 10:35:12 +0100

    streamlit updtate
    streamlit updtate

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -35,7 +35,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
-movies = pd.read_csv('resources/data/movies.csv', sep = ',')
+movies = pd.read_csv('../unsupervised_data/unsupervised_movie_data/movies.csv', sep = ',')
 ratings = pd.read_csv('resources/data/ratings.csv')
 movies.dropna(inplace=True)
 
