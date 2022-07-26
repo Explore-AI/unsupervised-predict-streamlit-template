06bd0980c6d7509f58034c278442d9e15dcbc494
Thulani Nyama <nyamathulani@gmail.com>
2022-07-26 20:59:28 +0200

    Pre-Final

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,17 +45,17 @@ over_theme = {'txc_inactive': '#FFFFFF'}
 
 # specify the primary menu definition
 menu_data = [
-    {'icon': "far fa-copy", 'label':"About"},
-    {'id':'Trailers','icon':"fas fa-film",'label':"Trailers"},
-    {'icon': "far fa-chart-bar", 'label':"Statistics"}, #no tooltip message
-    {'id':'Contact Us','icon': "fas fa-laptop", 'label':"Contact Us"},
-    {'id':'Help', 'icon': 'fas fa-question', 'label':"Help"}
+    {'icon': "far fa-copy", 'label':'About'},
+    {'id':'Trailers','icon':'fas fa-film','label':'Trailers'},
+    {'icon': 'far fa-chart-bar', 'label':'Statistics'}, #no tooltip message
+    {'id':'Contact Us','icon': 'fas fa-laptop', 'label':'Contact Us'},
+    {'id':'Help', 'icon': 'fas fa-question', 'label':'Help'}
 ]
 
 # App declaration
 
 def main():
-    
+    # define hydralit navbar
     menu_id = hc.nav_bar(
     menu_definition=menu_data,
     override_theme=over_theme,
@@ -65,7 +65,7 @@ def main():
     sticky_nav=True, #at the top or not
     sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
 )
-    page_selection = f"{menu_id}"
+    page_selection = f'{menu_id}'
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
     # page_options = ["Recommender System", "About", "Trailers", "Statistics", "The Dream Team", "Help Page"]
@@ -74,7 +74,7 @@ def main():
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
     # page_selection = st.sidebar.selectbox("Choose Option", page_options)
-    if page_selection == "Home":
+    if page_selection == 'Home':
         # Header contents
         st.write('# Movie Xplorer')
         # st.write('### EXPLORE Data Science Academy Unsupervised Predict')
@@ -82,7 +82,7 @@ def main():
         # Recommender System algorithm selection
         st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: right;} </style>', unsafe_allow_html=True)
         st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-right:2px;}</style>', unsafe_allow_html=True)
-        sys = st.radio("", ("Content Based Filtering", "Collaborative Based Filtering"))
+        sys = st.radio("", ('Content Based Filtering', 'Collaborative Based Filtering'))
 
         # User-based preferences
         st.write('### Select Your Three Favorite Movies')
@@ -93,29 +93,34 @@ def main():
 
         # Perform top-10 movie recommendation generation
         if sys == 'Content Based Filtering':
-            if st.button("Recommend"):
+            if st.button('Recommend'):
                 try:
+                    # intialize hydralit loaders
                     with hc.HyLoader('We\'re getting movies only you will love...\n',hc.Loaders.standard_loaders,index=[5,0,3]):
+                        # get top 10 recommended movies using the content_model algorithm
                         top_recommendations = content_model(movie_list=fav_movies, top_n=10)
                         time.sleep(5)
-                    st.title("Only you will love these movies...")
+                    st.title('Only you will love these movies...')
                     for i,j in enumerate(top_recommendations):
                         st.subheader(str(i+1)+'. '+j)
-                        # the alteration is here
+                        # get trailer from youtube
                         top_trailers.youtubeScrapper(top_recommendations[i])
                 except:
                     st.error("Oops! Looks like this algorithm does't work.\
                               We'll need to fix it!")
 
         if sys == 'Collaborative Based Filtering':
-            if st.button("Recommend"):
+            if st.button('Recommend'):
                 try:
+                    # intialize hydralit loaders
                     with hc.HyLoader('We\'re getting movies only you will love...\n',hc.Loaders.standard_loaders,index=[5,0,3]):
+                        # get top 10 recommended movies using the collab_model algorithm
                         top_recommendations = collab_model(movie_list=fav_movies, top_n=10)
                         time.sleep(5)
-                    st.title("Only you will love these movies...")
+                    st.title('Only you will love these movies...')
                     for i,j in enumerate(top_recommendations):
                         st.subheader(str(i+1)+'. '+j)
+                        # get trailer from youtube
                         top_trailers.youtubeScrapper(top_recommendations[i])
                 except:
                     st.error("Oops! Looks like this algorithm does't work.\
@@ -123,15 +128,20 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------------
-    elif page_selection == "About":
+    elif page_selection == 'About':
+        # navigate to the About page
         a.about()
-    elif page_selection == "Trailers":
+    elif page_selection == 'Trailers':
+        # navigate to the Trailers page
         t.vids()
-    elif page_selection == "Contact Us":
+    elif page_selection == 'Contact Us':
+        # navigate to the Contact Us page
         dreamers.data_professionals()
-    elif page_selection == "Statistics":
+    elif page_selection == 'Statistics':
+        # navigate to the Statistics page
         stat.visuals()
-    elif page_selection == "Help":
+    elif page_selection == 'Help':
+        # navigate to the Help page
         h.helppage()
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
--- menu/statistics.py
+++ menu/statistics.py
@@ -11,7 +11,8 @@ import base64
 import string
 import re
 movies = pd.read_csv('https://raw.githubusercontent.com/Dream-Team-Unsupervised/Data/main/movies.csv')
-imdb_data_budget = pd.read_csv('https://raw.githubusercontent.com/Dream-Team-Unsupervised/Data/main/imdb_data.csv')
+imdb = pd.read_csv('https://raw.githubusercontent.com/Dream-Team-Unsupervised/Data/main/imdb_data.csv')
+imdb_data_budget = imdb['runtime'].to_list()
 ratings = pd.read_csv('resources/data/ratings.csv')
 movies = movies.dropna() 
 @st.cache(allow_output_mutation=True)
@@ -44,87 +45,57 @@ def visuals():
 
         st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: right;} </style>', unsafe_allow_html=True)
         st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
-        chart = st.radio("",("Bar Chart","Line Chart","Box Plot"))
+        chart = st.radio("",("Line & Bar Chart","Box Plot", "Pie Chart"))
     
-        if chart == "Bar Chart":
-                with st.expander("Bar Chart"):
-                        options = st.multiselect('Select data', ['titles', 'genres'])
-                        st.write(options)
+        if chart == "Pie Chart":
+                with st.expander("Pie Chart"):
+                        options = st.multiselect('Select 2 metrics to plot', ['Genres', 'Top Rated'])
+                        if 'Genres' in options and 'Top Rated' in options:
+                                st.image('./resources/imgs/genres.png')
+                                st.info("We can see the most popular movie genres rated and drama is dramatically the most popular with 16.2%")
                 
-        elif chart == "Line Chart":
-            with st.expander("Line Chart"):
-                options = st.multiselect('Select data', ['Genres', 'Ratings', 'Years', 'Budget'])
+        elif chart == "Line & Bar Chart":
+                with st.expander("Line & Bar Chart"):
+                        options = st.multiselect('Select 2 metrics to plot', ['Movies', 'Distribution'])
+                        if 'Movies' in options and 'Distribution' in options:
+                                st.markdown("### Moivie KPIs 1900-2022")
+                                kpi2, kpi1, kpi3 = st.columns(3)
 
-                if 'Years' in options:
-                        st.markdown("### KPI 1900-2022")
-                        kpi1, kpi2, kpi3 = st.columns(3)
+                                my_dynamic_value = 3716.5 
 
-                        my_dynamic_value = 333.3335 
+                                new_val = 222
 
-                        new_val = 222
+                                final_val = my_dynamic_value / new_val
+                                
+                                kpi1.metric(label = "Avg released movies",
+                                        value = 3716.5,
+                                        delta = 1411)
 
-                        final_val = my_dynamic_value / new_val
-
-                        kpi1.metric(label = "Avg released movies",
-                                value = 3716.5,
-                                delta = 1411)
-
-                        kpi2.metric(label = "Bounce Rate",
-                                value = 78,
-                                delta = -5,
-                                delta_color = 'inverse')
-                        kpi3.metric(label = "Unique Visitors",
-                                value = "%.2f" %final_val )
+                                kpi3.metric(label = "Distribution",
+                                        value = "%.2f" %final_val )
+                        
+                                movies['year'] = [x[-1].strip('()') for x in movies.title.str.split(" ")]
                 
-                        movies['year'] = [x[-1].strip('()') for x in movies.title.str.split(" ")]
+                                num_pattern = r'^$|[a-zA-Z]|Τσιτσάνης|101次求婚|2006–2007|выбывание|پدر|Начальник|Джа|Девочки|первого'
+                                movies["year"] = movies["year"].replace(to_replace = num_pattern, value = np.nan, regex = True)
+                                year = [int(x) for x in movies["year"].dropna()]
+                                fig = plt.figure(figsize=(9,3))
+                                sns.histplot(year, kde = True,color = '#FF4B4B')
+                                plt.xlabel('Year')
+                                plt.xlim(left=1900, right = 2022)
+                                plt.title('Movie Release Year Distribution', fontweight = 'bold')
+                                st.pyplot(fig)
+                                st.info(f'  Our algorithms recommend from few 90s movie classics and a significant number 21st century movies')
         
-                        num_pattern = r'^$|[a-zA-Z]|Τσιτσάνης|101次求婚|2006–2007|выбывание|پدر|Начальник|Джа|Девочки|первого'
-                        movies["year"] = movies["year"].replace(to_replace = num_pattern, value = np.nan, regex = True)
-                        year = [int(x) for x in movies["year"].dropna()]
-                        fig = plt.figure(figsize=(9,3))
-                        sns.histplot(year, kde = True,color = '#FF4B4B')
-                        plt.xlabel('Year')
-                        plt.xlim(left=1900, right = 2022)
-                        plt.title('Movie Release Year Distribution', fontweight = 'bold')
-                        st.pyplot(fig)
-                        st.info(f'Our algorithms recommend from few 90s movie classics and a significant number 21st century movies')
-                elif 'Budget' in options:
-                        # budget distribution plot
-                        st.markdown("<h1 style = 'text-align: center;'>Movie Budget Distribution</h1>", unsafe_allow_html=True)
-                        # punctuations = string.punctuation 
-                        # budget = []
-                        # for text in imdb_data_budget.to_list():
-                        #         budget.append(int(re.sub('^[a-zA-Z]+','',text.translate(str.maketrans(' ',' ', punctuations)))))
-                        #         budget_df  = pd.DataFrame(budget, columns = ['budget'])
-                        #         fig = plt.figure(figsize=(9,3))
-                        #         sns.histplot(budget_df['budget'], bins=10000, color = '#FF824E')
-                        #         plt.xlabel('Budget')
-                        #         plt.xlim(left=0,right = 250000000)
-                        #         plt.title('Movie Budget Distribution', fontweight = 'bold')
-                        #         st.pyplot(fig)
-                        #         st.write(f'Average rating  database: {round(np.mean(budget_df["budget"]),2)} with a skewness to the right.')
         elif chart == "Box Plot":
                 with st.expander("Box Plot"):
-                        options = st.multiselect('Select data', ['Ratings', 'Years'])
+                        options = st.multiselect('Select 2 metrics to plot', ['Ratings', 'Distribution'])
                         
-                        if 'Ratings' in options:
+                        if 'Ratings' in options and 'Distribution' in options:
                                 # plot movie ratings distribution 
                                 fig = plt.figure(figsize=(9,3))
                                 sns.boxplot(x = "rating", data=ratings, color = '#FF4B4B')
                                 plt.title('Movie Ratings Distribution', fontweight = 'bold')
                                 plt.show()
                                 st.pyplot(fig)
-                                st.info(f'Average rating  database: {round(np.mean(ratings["rating"]),2)} with 75% of the rating greater than 3.')
-#     st.write("Due to the popularity of movies since 1974 we can definitely agree that there has been a influx of movies per annum.")
-#     # st.image('./resources/imgs/stats.gif')
-#     st.image('./resources/imgs/newplot2.png')
-#     st.markdown("")
-#     st.write("Below you can find the most popular genres rated for #appname") 
-#     st.image('./resources/imgs/newplot3.png')
-#     st.markdown("")
-#     st.write("")
-#     st.image('./resources/imgs/newplot4.png')
-#     st.markdown("")
-#     st.write("")
-#     st.image('./resources/imgs/newplot5.png')
- # TODO: this should probably display visuals without returning anything
+                                st.info(f'Average rating  distribution is {round(np.mean(ratings["rating"]),2)} with 75% of the ratings greater than 3.')
