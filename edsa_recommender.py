7f361358d5921dc79c0fb7c2dda63b1cda685955
Oluwaleke Oni <oluwalekeoni@gmail.com>
2022-08-24 16:13:14 +0100

    image added

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -66,21 +66,9 @@ def main():
 		
 	#Loading Company logo
 	st.sidebar.image('resources/imgs/logo.jpeg')
-	row1_space1, center_, row1_space2 = st.columns((.5, 1, .2,))
-	#with center_, _lock:
-	#	file_ = open('resources/imgs/logo.gif', "rb")
-	#	contents = file_.read()
-	#	data_url = base64.b64encode(contents).decode("utf-8")
-	#	file_.close()
-	#	st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">', unsafe_allow_html=True, )
-		
-		
+	row1_space1, center_, row1_space2 = st.columns((.5, 1, .2,))	
 
 		
-
-
-	   
-
 	# DO NOT REMOVE the 'Recommender System' option below, however,
 	# you are welcome to add more options to enrich your app.
 	page_options = ["Recommender System","Movie Dataset Visualization","Company Information & Background", "Project Team"]
