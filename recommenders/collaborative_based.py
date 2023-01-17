521ac7836f473e82b6b555d6bceca241438c0262
mugashdk <mugambiheavy@gmail.com>
2023-01-17 17:12:22 +0300

    connecting to data

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -38,12 +38,12 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
-movies_df = pd.read_csv('resources/data/movies.csv',sep = ',')
-ratings_df = pd.read_csv('resources/data/ratings.csv')
+movies_df = pd.read_csv('../resources/data/movies.csv',sep = ',')
+ratings_df = pd.read_csv('../resources/data/ratings.csv')
 ratings_df.drop(['timestamp'], axis=1,inplace=True)
 
 # We make use of an SVD model trained on a subset of the MovieLens 10k dataset.
-model=pickle.load(open('resources/models/SVD.pkl', 'rb'))
+model=pickle.load(open('../resources/models/SVD.pkl', 'rb'))
 
 def prediction_item(item_id):
     """Map a given favourite movie to users within the
