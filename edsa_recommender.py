c4c5f57fbaa24663de3cda548ea7be2b80dbc38c
nmraqisa <112183163+nmraqisa@users.noreply.github.com>
2023-01-27 00:06:55 +0200

    EDA

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -30,6 +30,8 @@ import streamlit as st
 import PIL.Image
 import os
 from tkinter import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Data handling dependencies
 import pandas as pd
@@ -72,7 +74,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Solution Overview", "EDA"]
     image = PIL.Image.open(os.path.join("resources/imgs/team_logo.jpg"))
     st.image(image, width=300, caption='')
     # -------------------------------------------------------------------
@@ -133,6 +135,53 @@ def main():
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
+
+    if page_selection == "EDA":
+        # Header contents
+        st.title("Exploratory data Analysis")
+        #st.image('resources/imgs/Image_header.png',use_column_width=True)
+        st.write('## Gather additional insights')
+        #Create selectbox for EDA components
+        eda_options = ["Genres","Movies","Users"]
+        eda_selection = st.selectbox("Select the insights to be explored", eda_options)
+
+        #Define functions to create plots
+        def plot_most_frequent(title,df,y_lab,x_lab,top,order=False):
+            """
+            This function plots a count plot
+            """
+            plt.figure(figsize=(15,10),tight_layout=True)
+            sns.countplot(y=y_lab, data=df,
+                        order=df[y_lab].value_counts(ascending=order)[0:top].index,palette='Blues_r')
+            plt.xlabel('Count', fontsize=30)
+            plt.ylabel(y_lab.capitalize(), fontsize=30)
+            plt.yticks(fontsize=20)
+            plt.xticks(fontsize=20)
+            plt.show()
+
+        def bar_plot_func(title,df,y_lab,x_lab,top,order=False):
+            """
+            This function plots a bar plot of the words
+            """
+
+            plt.figure(figsize=(15,10),tight_layout=True)
+            sns.barplot(x = x_lab, y = y_lab,
+                        data = df.sort_values(x_lab,ascending = order)[0:top],
+                        palette = 'Blues_r')
+            plt.xlabel(x_lab.capitalize(), fontsize=12)
+            plt.ylabel("", fontsize=12)
+            plt.yticks(fontsize=20)
+            plt.xticks(fontsize=12)
+            plt.show()
+
+        # Create a bar plot of the top 20 best rated movies
+        #fig, ax  = plt.subplots(1,2,figsize=(20,10))
+        #sns.barplot(ax=ax[0], x='rating',y = 'title', data=movies.head(20)) 
+        #ax[0].set_title('The top 20 best rated movies ') 
+
+
+        #st.title("EDA Overview")
+        #st.write("Describe your winning approach on this page")    
     
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
