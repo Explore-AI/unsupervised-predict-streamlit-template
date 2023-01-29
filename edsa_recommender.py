fe1bd565106f1eb42452547b66212fc5c94a5732
Your Name <Malik>
2023-01-29 14:33:21 +0100

    Added new pages.

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Explore Movies", "Solution Overview", "About Us", "Contact Us"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -100,9 +100,94 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    # Explore Movies  
+    if page_selection == "Explore Movies":
+        st.title("Explore Movies")
+
+        search = st.text_input("Search for a movie")
+        if st.button("Search"):
+            Print("")
+            # Perform movie search here
+        
+        # Read in the ratings and movies datasets
+        ratings = pd.read_csv("resources/data/ratings.csv")
+        movies = pd.read_csv("resources/data/movies.csv")
+
+        # Merge the ratings and movies datasets on the movieId column
+        merged_df = pd.merge(ratings, movies, on='movieId')
+
+        # Group the merged dataset by movie title and calculate the average rating for each movie
+        grouped_df = merged_df.groupby('title')['rating'].mean().reset_index()
+
+        # Sort the grouped dataset by average rating in descending order and select the top 10 movies
+        top_rated_movies = grouped_df.sort_values(by='rating', ascending=False).head(10)
+
+        st.markdown("### Top Rated Movies")
+
+        # Create a horizontal row to display the top-rated movies
+       
+        for i, movie in enumerate(top_rated_movies['title']):
+            st.write(f"{i+1}. {movie}")
+      
+    # Solution Overview
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
+        st.write("Our movie recommender system uses a content-based and collaborative-based filtering approaches to recommend movies to users. The system takes in a user's input of a movie title, processes the data using the cosine similarity algorithm, and then recommends the top 10 most similar movies based on the user's input.")
+        st.markdown("""
+    Before building the recommendation model, we conducted an extensive Exploratory Data Analysis (EDA) on a dataset of movies. 
+    During the EDA, we cleaned and preprocessed the data, and performed various visualizations to understand the distribution and relationships of the features in the dataset.
+    """)
+
+        st.markdown("""
+    Based on the insights gained from the EDA, we identified the important features that would be used as inputs for the recommendation model.
+    These features include the movie's **genres, plot keywords, cast, directors and ratings**. By using these features, the model is able to understand the user's preferences and make recommendations that are tailored to their interests or interests of someone similar.
+    """)
+        st.markdown(""" Here are some key insights we drew from our EDA:""")
+        
+        st.markdown("- The majority of movies in our dataset are rated as 'average' by critics, with only a small percentage being highly rated or poorly rated.")
+        st.markdown("- The majority of movies are English-language films, but there is a significant number of foreign-language films as well.")
+        st.markdown("- Action and Adventure are the most popular genres in our dataset, followed by Comedy and Thriller.")
+        st.markdown("- The majority of movies in our dataset have a runtime of between 90-120 minutes.")
+        st.markdown("- There is a positive correlation between the budget of a movie and its revenue.")    
+        st.markdown("""The solution was designed and developed by the team of **MovieGenius** company.""")     
+        
+        # About Us
+    if page_selection == "About Us":
+        st.header("About Us")
+        st.write("Our team consists of experienced data scientists and engineers who are passionate about using technology to enhance the movie-watching experience. We at Movie Magic Inc. strive to provide the best movie recommendations to our users by constantly updating and improving our model.")
+        
+        st.write("Our team members:")
+        st.write("- Malik Kabir, Team Lead")
+        st.write("- Mariam M'mbetsa, Machine Learning Engineer")
+        st.write("- Mahlatse Motlanthi, Data Scientist")
+        st.write("- Haruna Jibrin, Data Analyst")
+        st.write("- Babajide Adelekan, Business Analyst")
+        st.write("- Ncedo Fakude, Business Analyst II")
+        
+        st.write("Our movie recommendation system is based on a state-of-the-art content-based filtering algorithm. This approach takes into account the features of the movie such as the cast, crew, plot, and keywords to provide personalized and accurate recommendations.")
+        st.write("We have a vast movie database that is constantly updated to ensure that you always have access to the latest releases and hidden gems.")
+        st.write("In addition, we offer a user-friendly web interface that allows you to easily search for movies and view our top recommendations.")
+        st.markdown("""
+    Feel free to **Contact Us** for any further information.
+    """)
+
+        st.success("Exploratory Data Analysis (EDA)")
+        st.info("- Cleaning and preprocessing of the data")
+        st.info("- Understanding distribution and relationships of the features in the dataset")
+        st.info("- Identifying important features to be used as inputs for the recommendation model")
+
+        st.image('https://www.example.com/images/EDA.png', width=600)
+        st.write("Thank you for choosing MovieGenius for all your movie recommendation needs!")
+        
+        # Contact Us
+    if page_selection == "Contact Us":
+        st.header("Contact Us")
+        st.write("We would love to hear from you!")
+        st.write("Email us at contact@moviegenius.com")
+        st.write("Follow us on social media:")
+        st.write("Twitter: @moviegenius")
+        st.write("Facebook: /moviegenius")
+        st.write("Instagram: @moviegenius")
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
