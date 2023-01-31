955a2bd7136e08a8e78e4c30810e8a7b481037ee
tshegofatsoMaf <tshegofatso.mafuna@gmail.com>
2023-01-31 17:28:08 +0200

    Claring conflict

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -254,47 +254,4 @@ def main():
 
     if page_selection == "Contact us":
 
-        
-        def add_bg_from_url():
-         st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://images.unsplash.com/photo-1475274047050-1d0c0975c63e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80.jpg");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-
-        add_bg_from_url() 
-        
-                  
-        contact_pic = "https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')"
-        st.image(contact_pic, caption='telephone')
-
-        st.text_input(
-        "Tell us more on where we can improve. Feel free to write below or send us an email.",
-        "Tell us ",
-        key="placeholder",
-
-
-    )
-
-
-        st.title("Email us")
-        st.write(" stalink.za@gmail.com")
-
-        contact_op = st.selectbox(
-        'How would you like to be contacted?',
-        ('Email', 'Home phone', 'Mobile phone'))
-
-        st.write('You selected:', contact_op)
-    # You may want to add more sections here for aspects such as an EDA,
-    # or to provide your business pitch.
-
-
-if __name__ == '__main__':
-    main()
+      
