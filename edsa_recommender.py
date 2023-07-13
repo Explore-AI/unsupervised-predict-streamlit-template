2c4582b1efb9bea7d397dc59fec3e3957b280d10
OBOT JOSHUA PETER <joshua.shiloh88@gmail.com>
2023-07-14 00:19:39 +0100

    Added home and about us page
    A new commit from Obot Joshua Peter

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["HOME","ABOUT US","Recommender System","Solution Overview"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
