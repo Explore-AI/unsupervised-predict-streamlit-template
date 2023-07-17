1dddcefce09549b5b01aa722b9ce17f3b20c7152
Matsawela-Precious <132572106+Matsawela-Precious@users.noreply.github.com>
2023-07-17 11:34:31 +0200

    Add files via upload

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -27,6 +27,8 @@
 """
 # Streamlit dependencies
 import streamlit as st
+import streamlit.components.v1 as components
+import base64
 
 # Data handling dependencies
 import pandas as pd
@@ -39,19 +41,87 @@ from recommenders.content_based import content_model
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
+ 
+font_css = """
+            <style>
+            div[data-baseweb="tab-list"] {
+                background:rgba(246, 246, 246, 0.4);
+                padding: 0 5px 0 5px;
+                border-radius: 5px;
+            }
+            
+            button[data-baseweb="tab"] > div[data-testid="stMarkdownContainer"] > p {
+              font-size: 18px;
+            }
+            
+            button[data-baseweb="tab"] {
+                background: transparent
+            }
+            
+            button[data-baseweb="tab"]:hover {
+                background: rgba(230, 230, 230, 0.4);
+                color: rgb(255, 255, 255)
+            }
+            
+            div[data-baseweb="tab-highlight"] {
+                background: rgba(230, 230, 230, 0.4);
+                color: rgb(255, 255, 255)
+            }
+            
+            button[data-baseweb="tab"]:active {
+                background: rgba(230, 230, 230, 0.4);
+                color: rgb(255, 255, 255)
+            }
+            
+            div[data-testid="stExpander"] {
+                background:rgba(246, 246, 246, 0.4);
+                border-radius: 5px;
+                box-shadow: 2px 2px;
+            }
+
+            </style>
+            """
+st.markdown(font_css, unsafe_allow_html=True) 
 
 # App declaration
 def main():
 
+    def add_bg_from_local(image_file):
+        with open(image_file, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        st.markdown(
+        f"""
+            <style>
+                .stApp {{
+                    background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+                    background-size: cover
+                }}
+            </style>
+        """,
+        unsafe_allow_html=True
+        )
+        
+    add_bg_from_local('C:/Users/44475/example2/resources/imgs/back.jpg')
+
+    with st.sidebar:
+        st.image("resources/imgs/Flix.png", width=200)
+    st.sidebar.title("Movie Recommendation System")
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Home","Recommender System","Solution Overview", "About","Contact Us" ]
+    page_options = ["Home","Recommender System","Analysis", "About Us","Info", "Feedback"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
+
+ 
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
+    
+
     if page_selection == "Recommender System":
+
+  
+        
         # Header contents
         st.write('# Movie Recommender Engine')
         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
@@ -100,65 +170,110 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
         
-        
+         
         
     # -------------------------------------------------------------------
 
-    # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    def set_background_image():
-        st.markdown(
-            """
-            <style>
-            .reportview-container {
-                background: url('https://www.bing.com/images/search?view=detailV2&ccid=lss9flZL&id=9391FDB2991E917F20C692D8752FFF45F0895BA9&thid=OIP.lss9flZLDjAnoZ_Qa_sm1wHaFP&mediaurl=https%3a%2f%2faihubprojects.com%2fwp-content%2fuploads%2f2019%2f10%2fimage-6.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.96cb3d7e564b0e3027a19fd06bfb26d7%3frik%3dqVuJ8EX%252fL3XYkg%26pid%3dImgRaw%26r%3d0&exph=935&expw=1320&q=Movie+Recommender+System&simid=608046088341580220&FORM=IRPRST&ck=2C6BB31171A5E00DA46DD18A1CBD2BC7&selectedIndex=5') no-repeat center center fixed;
-                background-size: cover;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
-        
-    set_background_image()
+    # ------------- SAFE FOR ALTERING/EXTENSION -------------------------
 
     
-    def set_background_animation():
-        st.markdown(
-            """
-            <style>
-            @keyframes moving-background {
-                0% {
-                    background-position: 0% 50%;
-                }
-                50% {
-                    background-position: 100% 50%;
-                }
-                100% {
-                
-                
-                    background-position: 0% 50%;
-                }
-           }
-        
-           .reportview-container {
-            animation: moving-background 10s ease infinite;
-           }
-           </style>
-           """,
-           unsafe_allow_html=True
-       )
-        
-    set_background_animation()
 
+
+    if page_selection == "Feedback":
+
+        st.image('resources/imgs/Feedback.jpg', width = 300)
+    
+       
+        # Creating a text box for user input
+        feedback_text = st.text_area("","Type Here:")
+
+        if st.button("Submit Feedback"):
+            st.info("Feedback Submitted...")
+            feedback_text = 'Type Here'
 
     if page_selection == "Home":
-        st.title("Home")
-        st.write("Plenty Movies\
-                  For Everyone\
-                  To Enjoy Anywhere, Anytime")
+
+  
+      st.title("The ultimate app for EVERY movie enthusiasts")
+      st.write("")
+      st.image('resources/imgs/movie_background.jpg',use_column_width=True)
+      st.write("")
+      st.write("FlixWhiz is the perfect app for all your movie recommendations. It gives you the freedom of choosing the perfect movie to watch without wrecking your brain. Input your 3 favourite movies and viola!!")
+      st.title("Plenty Movies For Everyone To Enjoy Anywhere, Anytime")
+
+
+    # Building out the about us page
+    if page_selection == "About Us":
+
+        #st.markdown(font_css, unsafe_allow_html=True)
+        tab1, tab2, tab3 = st.tabs(["Bits And Bytes Data Solutions", "Testimonials", "Contact Us"])
+
+        with tab1:
+            st.write("")
+            st.image('resources/imgs/Logo.jpg',width=300)
+            st.write("")
+            st.write("Welcome to FlixWhiz, the ultimate destination for movie enthusiasts. Our passion lies in providing personalized movie recommendations that cater to your unique tastes and preferences. With our advanced recommendation algorithms, we analyze your favorite movies, genres, and more to curate a tailored selection of films just for you.")
+            st.write("At FlixWhiz, we have a dedicated team of movie lovers and data scientists who are committed to enhancing your movie-watching experience. We continuously update our extensive movie database, refine our algorithms, and incorporate user feedback to ensure that our recommendations are accurate and satisfying.")
+            st.write("Discovering the perfect movie has never been easier. Whether you're in the mood for action, romance, comedy, or any other genre, FlixWhiz is here to guide you. Join us on this cinematic journey and let FlixWhiz be your trusted companion for finding the next great film to enjoy.")
+
+
+        with tab2: 
+            st.write("")
+            st.image('resources/imgs/Test.jpg',width=300)
+            st.write("")
+
+            col8, col9 = st.columns(2)
+            with col8:
+                st.markdown("\"As a movie enthusiast, FlixWhiz is my go-to platform for finding new films to watch. The recommendations are always on point, and I appreciate the variety of genres and options available. It's like having my own personal movie curator!\" - Sarah White")
+             
+            with col9:
+                st.markdown("\"I've tried other movie recommendation platforms, but none come close to FlixWhiz. The accuracy of their recommendations is impressive, and the quality of the movies suggested is consistently high. I've discovered some of my all-time favorite films through this app.\" - Mpho Maremane")
+
+            st.write("")
+            col10, col11 = st.columns(2)
+            with col10:
+                st.markdown("\"FlixWhiz has revolutionized the way I discover movies. The personalized recommendations are spot-on, and I've discovered so many hidden gems that I wouldn't have found otherwise. Highly recommended!\" - John Schoeman")
+
+            with col11:
+                st.markdown("\"I love how easy and user-friendly FlixWhiz is. The interface is clean and intuitive, and the recommendations are tailored to my preferences. It's like having a virtual movie buddy who knows exactly what I'll enjoy!\" - Michael Reid")
+
+        with tab3:
+
+            col12, col13 = st.columns(2)
+            with col12:
+                # Contact Us Section
+                st.title("Contact Us")
+
+                # Input fields for name, email, and message
+                name = st.text_input("Your Name")
+                email = st.text_input("Your Email")
+                message = st.text_area("Message")
+
+                # Submit button
+                if st.button("Submit"):
+                   # Process the form data
+                   # You can add your own logic here, such as sending an email or storing the data in a database
+                   st.success("Thank you for your message! We will get back to you soon.")
+            
+                st.write("")
+            
+            
+            with col13:
+                st.title("Contact Details")
+                st.write("Office Line : (+27) 43 656 9800")
+                st.write("Emergency Line : (+27) 43 656 9000")
+                st.write("Email Address : info@bitsandbytes.co.za")
+                st.image("resources/imgs/Logo.jpg", width=300)
+
+
+
+
+
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
