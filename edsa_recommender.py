b6dd7e8cb23475c27295b11bba2134d5c3426340
Sandile Tsewu <sandile.tsewu98@gmail.com>
2023-07-22 18:14:05 +0200

    Added the second bar chat.

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -52,6 +52,7 @@ mov=pd.read_csv("resources/data/movie_details_average.csv")
 pie=pd.read_csv("resources/data/gen_only.csv")
 movie_df = pd.read_csv('resources/data/movie_insights_3.csv')
 tag_df = pd.read_csv('resources/data/tag_insights.csv')
+rating_count = pd.read_csv('resources/data/rating_count.csv')
 
 # App declaration
 def main():
@@ -116,16 +117,16 @@ def main():
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Instruction & Overview":
         st.title("Welcome to ***DATAMIN*** Movie Recommender App 🎥🍿")
-        st.image("resources/imgs/Movie-Recommendation-System-with-Streamlit-and-Python-ML-1.jpg",)
+        st.image("resources/imgs/Movie-Recommendation-System-with-Streamlit-and-Python-ML-1.jpg")
         st.write("Hi there🙋‍♂️, looking for that nice thing to watch without having to go over the hustle of scrolling for movies? Relax, we've got you.🤗")
         st.write("")
         st.subheader("About the App")
         st.write("This app helps you discover personalized movie recommendations based on your preferences.")
         st.write("On the left of this page you will find a side-bar with a dropdown menu for the pages available, these pages include:")
-        st.write("1. Instruction and Overview - The current page with the information about how to use this App.")
-        st.write("2. Recommender System - This is where you will get your recommended movies.")
-        st.write("3. Insights - This is were you get the insigts about the movies and genres. ")
-        st.write("4. Contact Us - This page has the App developer team information.")
+        st.write("1. **Instruction and Overview** - The current page with the information about how to use this App.")
+        st.write("2. **Recommender System** - This is where you will get your recommended movies.")
+        st.write("3. **Insights** - This is were you get the insigts about the movies and genres. ")
+        st.write("4. **Contact Us** - This page has the App developer team information.")
 
         
         # Defining a list of image paths
@@ -160,8 +161,8 @@ def main():
 
         st.subheader("How to Use the App")
         st.write("1. Navigate to the 'Recommender System' page.")
-        st.write("2. Select the type of filtering (Content-based or Collaborative) for you movie choices.")
-        st.write("2. Select you three favourated movies.")
+        st.write("2. Select the type of filtering (Content-based or Collaborative) for your movie choices.")
+        st.write("2. Select your three favourated movies.")
         st.write("2. Press 'Recommand'.")
         st.write("3. Receive personalized movie recommendations based on your favourite movies.")
 
@@ -171,6 +172,8 @@ def main():
     if page_selection == "Insights":
 
        # st.markdown("<h2 style=color:#3FBEBF;>Top Rated Movies By Genres</h2>",unsafe_allow_html=True)
+        # st.image("resources/imgs/insights-information.jpg",width=500)
+
 
         tab1,tab2,tab3=st.tabs(["Genres Insights","Movie Insights","Other Visuals"])
 
@@ -339,8 +342,8 @@ def main():
         with tab3:
 
             # Get unique years from the DataFrame
-            years = sorted(movie_df['year'].unique())
-
+            years = sorted(movie_df['year'].unique(), reverse=True)
+            st.subheader('Select a year to see the top 10 rated movies for that year.')
             # Creating a sidebar with the year dropdown
             selected_year = st.selectbox("Select a year", years)
 
@@ -363,7 +366,7 @@ def main():
             # Setting the x and y-axis labels and the title
             ax.set_xlabel('Rating', fontsize=16)   # Set the font size for x-axis label
             ax.set_ylabel('Movie Title', fontsize=16)  # Set the font size for y-axis label
-            ax.set_title(f'Top Movies of {selected_year}', fontsize=18)  # Set the font size for the title
+            ax.set_title(f'Top 10 Movies of {selected_year}', fontsize=18)  # Set the font size for the title
 
             # Display the chart
             plt.tight_layout()  # Ensures all elements fit within the figure area
@@ -375,10 +378,47 @@ def main():
             st.write("Top Ten Movie Titles:")
             st.table(top_movies_df[['title']])
 
-            # # Get the unique years from the DataFrame
-            unique_years = tag_df['year'].unique()
+            #---------------------------------------------------------------------------------------------------#
+            # Create a dropdown to allow the user to select a year
+            years = sorted(rating_count['year'].unique(), reverse=True)
+            st.subheader('Select a year to see movies with the highest number of users that rated them for that year')
+            selected_year = st.selectbox('Select a year', years)
+            
+            # Filter the merged DataFrame based on the selected year
+            filtered_df = rating_count[rating_count['year'] == selected_year]
 
-            st.subheader('Select a year below for the Word Cloud of tags asociated with the movies released in that year.')
+            # Group the filtered DataFrame by movie title and count the number of unique users
+            movie_ratings_count = filtered_df.groupby('title')['userId'].nunique().reset_index()
+            movie_ratings_count = movie_ratings_count.rename(columns={'userId': 'user_count'})
+
+            # Sort the movies based on rating count in descending order
+            movie_ratings_count = movie_ratings_count.sort_values(by='user_count', ascending=False)
+
+            # Select only the top 10 movies (if there are more than 10)
+            if len(movie_ratings_count) > 10:
+                movie_ratings_count = movie_ratings_count.head(10)
+
+            # Create a horizontal bar chart
+            fig, ax = plt.subplots(figsize=(10, 8))
+            ax.barh(movie_ratings_count['title'], movie_ratings_count['user_count'], color='skyblue')
+
+            # Setting the font size for x and y-axis labels
+            ax.tick_params(axis='x', labelsize=12)  # Set the font size for x-axis labels
+            ax.tick_params(axis='y', labelsize=12)  # Set the font size for y-axis labels
+
+            ax.set_xlabel('Number of Users',fontsize=14)
+            ax.set_ylabel('Movie Title',fontsize=14)
+            ax.set_title(f'Top 10 Movies with Highest Rating Count in {selected_year}',fontsize=16)
+            plt.tight_layout()
+
+            # Display the bar chart in Streamlit
+            st.pyplot(fig)
+
+            #--------------------------------------------------------------------------------------------------------#
+            # # Get the unique years from the DataFrame
+            unique_years = sorted(tag_df['year'].unique(), reverse=True)
+
+            st.subheader('Select a year below for the Word Cloud of tags associated with the movies released in that year.')
 
             # Ask the user to select a year from the available options
             selected_year = st.selectbox("Select a year:", unique_years)
@@ -390,7 +430,7 @@ def main():
             tags_text = " ".join(selected_tags)
 
             # Create a WordCloud based on the combined tags text
-            wordcloud = WordCloud(width=800, height=400, background_color='black').generate(tags_text)
+            wordcloud = WordCloud(width=900, height=500, background_color='black').generate(tags_text)
 
             # Display the WordCloud using Streamlit
             st.title(f"Word Cloud for Movies in {selected_year}")
@@ -398,6 +438,7 @@ def main():
             plt.imshow(wordcloud, interpolation='bilinear')
             plt.axis('off')
             st.pyplot(plt)
+
 
     if page_selection == "Contact Us":
                 # Define the company details
