aadff64970383858221c2908e65bd9123b6c7456
Sandile Tsewu <sandile.tsewu98@gmail.com>
2023-07-23 17:04:04 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -88,9 +88,9 @@ def main():
 
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[1493:1520])
-        movie_2 = st.selectbox('Second Option',title_list[2505:2525])
-        movie_3 = st.selectbox('Third Option',title_list[2110:2120])
+        movie_1 = st.selectbox('Fisrt Option',title_list[149:152])
+        movie_2 = st.selectbox('Second Option',title_list[250:252])
+        movie_3 = st.selectbox('Third Option',title_list[211:212])
         fav_movies = [movie_1,movie_2,movie_3]
 
         # Perform top-10 movie recommendation generation
