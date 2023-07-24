6a7a48d8bf8194925ca8675701cf3530e1d505f5
Mmabatho-08 <126807713+Mmabatho-08@users.noreply.github.com>
2023-07-24 22:38:52 +0200

    Add files via upload
    EDA added

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -28,15 +28,15 @@
 # Streamlit dependencies
 import streamlit as st
 import streamlit.components.v1 as components
-
 import os
-import base64
+import pickle
 
 
 # Data handling dependencies
 import pandas as pd
 import numpy as np
 import base64
+
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
@@ -47,47 +47,8 @@ from recommenders.content_based import content_model
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
- 
-font_css = """
-            <style>
-            div[data-baseweb="tab-list"] {
-                background:rgba(246, 246, 246, 0.4);
-                padding: 0 5px 0 5px;
-                border-radius: 5px;
-            }
-            
-            button[data-baseweb="tab"] > div[data-testid="stMarkdownContainer"] > p {
-              font-size: 18px;
-            }
-            
-            button[data-baseweb="tab"] {
-                background: transparent
-            }
-            
-            button[data-baseweb="tab"]:hover {
-                background: rgba(230, 230, 230, 0.4);
-                color: rgb(255, 255, 255)
-            }
-            
-            div[data-baseweb="tab-highlight"] {
-                background: rgba(230, 230, 230, 0.4);
-                color: rgb(255, 255, 255)
-            }
-            
-            button[data-baseweb="tab"]:active {
-                background: rgba(230, 230, 230, 0.4);
-                color: rgb(255, 255, 255)
-            }
-            
-            div[data-testid="stExpander"] {
-                background:rgba(246, 246, 246, 0.4);
-                border-radius: 5px;
-                box-shadow: 2px 2px;
-            }
-
-            </style>
-            """
-st.markdown(font_css, unsafe_allow_html=True) 
+with open('resources/models/SVD.pkl', 'rb') as file:
+    SVD = pickle.load(file)
 
 # Setting up theme
 font_css = """
@@ -150,19 +111,15 @@ def main():
                     background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
                     background-size: cover
                 }}
-
                 .css-17eq0hr {{  /* Replace 'css-17eq0hr' with the actual CSS class for the sidebar */
                 background-color: transparent;
             }}
                 
-
-
             </style>
         """,
         unsafe_allow_html=True
         )
         
-
     add_bg_from_local('resources/imgs/red6.jpg')
 
 
@@ -187,39 +144,17 @@ st.sidebar.title("Movie Recommendation System")
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-page_options = ["Home","Recommender System","Analysis", "About Us", "FlixWhiz Info", "Feedback"]
+page_options = ["Home", "FlixWhiz Info", "Recommender System","Analysis", "About Us", "Feedback"]
 
 # with st.sidebar:
 #         st.image("resources/imgs/1.png", width=300)
 #         st.sidebar.title("Movie Recommendation System")
 
-    add_bg_from_local('resources/imgs/istockphoto.jpg')
-
-    with st.sidebar:
-        st.image("resources/imgs/Flix.png", width=200)
-    st.sidebar.title("Movie Recommendation System")
-    # DO NOT REMOVE the 'Recommender System' option below, however,
-    # you are welcome to add more options to enrich your app.
-    page_options = ["Home","Recommender System","Analysis", "About Us","FlixWhiz Info", "Feedback"]
-
-
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
-
 page_selection = st.sidebar.selectbox("Choose Option", page_options)
 if page_selection == "Recommender System":
-
-
- 
-    page_selection = st.sidebar.selectbox("Choose Option", page_options)
-    
-
-    if page_selection == "Recommender System":
-
-  
-        
-
         # Header contents
         st.write('# Movie Recommender Engine')
         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
@@ -271,199 +206,11 @@ if page_selection == "Recommender System":
 
 
 
-
     # if page_selection == "Solution Overview":
     #     st.title("Solution Overview")
     #     st.image("resources/imgs/winner.jpg")
         
     #     st.write("Describe your winning approach on this page")
-
-    
-    if page_selection == "Solution Overview":
-        st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
-        
-         
-        
-    # -------------------------------------------------------------------
-
-    # ------------- SAFE FOR ALTERING/EXTENSION -------------------------
-
-    
-
-
-    if page_selection == "Feedback":
-
-        st.image('resources/imgs/Feedback.jpg', width = 300)
-    
-       
-        # Creating a text box for user input
-        feedback_text = st.text_area("","Type Here:")
-
-        if st.button("Submit Feedback"):
-            st.info("Feedback Submitted...")
-            feedback_text = 'Type Here'
-
-    if page_selection == "Home":
-
-  
-      st.title("The ultimate app for EVERY movie enthusiasts")
-      st.write("")
-      st.image('resources/imgs/red-white-bucket.jpg',use_column_width=True)
-      st.write("")
-      st.write("FlixWhiz is the perfect app for all your movie recommendations. It gives you the freedom of choosing the perfect movie to watch without wrecking your brain. Input your 3 favourite movies and viola!!")
-      st.title("Plenty Movies For Everyone To Enjoy Anywhere, Anytime")
-
-
-    # Building out the about us page
-    if page_selection == "About Us":
-
-        #st.markdown(font_css, unsafe_allow_html=True)
-        tab1, tab2, tab3, tab4 = st.tabs(["Bits And Bytes Data Solutions", "Testimonials", "Contact Us", "The Team"])
-
-        with tab1:
-            st.write("")
-            st.image('resources/imgs/Logo.jpg',width=300)
-            st.write("")
-            st.write("Welcome to FlixWhiz, the ultimate destination for movie enthusiasts. Our passion lies in providing personalized movie recommendations that cater to your unique tastes and preferences. With our advanced recommendation algorithms, we analyze your favorite movies, genres, and more to curate a tailored selection of films just for you.")
-            st.write("At FlixWhiz, we have a dedicated team of movie lovers and data scientists who are committed to enhancing your movie-watching experience. We continuously update our extensive movie database, refine our algorithms, and incorporate user feedback to ensure that our recommendations are accurate and satisfying.")
-            st.write("Discovering the perfect movie has never been easier. Whether you're in the mood for action, romance, comedy, or any other genre, FlixWhiz is here to guide you. Join us on this cinematic journey and let FlixWhiz be your trusted companion for finding the next great film to enjoy.")
-
-
-        with tab2: 
-            st.write("")
-            st.image('resources/imgs/Test.jpg',width=300)
-            st.write("")
-
-            col8, col9 = st.columns(2)
-            with col8:
-                st.markdown("\"As a movie enthusiast, FlixWhiz is my go-to platform for finding new films to watch. The recommendations are always on point, and I appreciate the variety of genres and options available. It's like having my own personal movie curator!\" - Sarah White")
-             
-            with col9:
-                st.markdown("\"I've tried other movie recommendation platforms, but none come close to FlixWhiz. The accuracy of their recommendations is impressive, and the quality of the movies suggested is consistently high. I've discovered some of my all-time favorite films through this app.\" - Mpho Maremane")
-
-            st.write("")
-            col10, col11 = st.columns(2)
-            with col10:
-                st.markdown("\"FlixWhiz has revolutionized the way I discover movies. The personalized recommendations are spot-on, and I've discovered so many hidden gems that I wouldn't have found otherwise. Highly recommended!\" - John Schoeman")
-
-            with col11:
-                st.markdown("\"I love how easy and user-friendly FlixWhiz is. The interface is clean and intuitive, and the recommendations are tailored to my preferences. It's like having a virtual movie buddy who knows exactly what I'll enjoy!\" - Michael Reid")
-
-        with tab3:
-
-            col12, col13 = st.columns(2)
-            with col12:
-                # Contact Us Section
-                st.title("Contact Us")
-
-                # Input fields for name, email, and message
-                name = st.text_input("Your Name")
-                email = st.text_input("Your Email")
-                message = st.text_area("Message")
-
-                # Submit button
-                if st.button("Submit"):
-                   # Process the form data
-                   # You can add your own logic here, such as sending an email or storing the data in a database
-                   st.success("Thank you for your message! We will get back to you soon.")
-            
-                st.write("")
-            
-        with tab4:
-            #st.write("# MEET THE TEAM")
-            st.markdown("<p style='text-align: center; font-size: 80px; font-family: cursive; font-weight: bold; '>MEET THE TEAM</p>", unsafe_allow_html=True)
-            #st.image('resources/imgs/meet-team.jpg') 
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-
-            with st.container():
-                st.write("<p style='text-align: center; font-size: 50px; '>The Notebook Team</p>", unsafe_allow_html=True)
-				
-                with st.container():
-                    
-                    col1, col2, col3 = st.columns(3)
-				
-                with col1:
-                    st.image('resources/imgs/W.png')
-                    st.write("<p style='text-align: center; font-size: 20px; font-family: cursive; '>Rinae Surname</p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Occupation: </p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Current Favorite Movie:  </p>", unsafe_allow_html=True)
-					
-                with col2:
-                    st.image('resources/imgs/M.png')
-                    st.write("<p style='text-align: center; font-size: 20px; font-family: cursive; '>Nassau Surname</p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Occupation: </p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Current Favorite Movie:  </p>", unsafe_allow_html=True)
-						
-                with col3:
-                    st.image('resources/imgs/M.png')
-                    st.write("<p style='text-align: center; font-size: 20px; font-family: cursive; '>Bonolo Surname</p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Occupation: </p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Current Favorite Movie:  </p>", unsafe_allow_html=True)
-                    st.write("")
-                    st.write("")
-                    
-                            
-                with st.container():
-                    st.write("<p style='text-align: center; font-size: 50px; '>The Strealit Team</p>", unsafe_allow_html=True)
-                    
-                    with st.container():
-                    
-                        col1, col2, col3 = st.columns(3)
-				
-                with col1:
-                    st.image('resources/imgs/W.png')
-                    st.write("<p style='text-align: center; font-size: 20px; font-family: cursive; '>Matsawela Surname</p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Occupation: </p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Current Favorite Movie:  </p>", unsafe_allow_html=True)
-					
-                    with col2:
-                        st.image('resources/imgs/w.png')
-                        st.write("<p style='text-align: center; font-size: 20px; font-family: cursive; '>Mmabatho Surname</p>", unsafe_allow_html=True)
-                        st.write("<p style='text-align: center; font-size: 12px; '>Occupation: </p>", unsafe_allow_html=True)
-                        st.write("<p style='text-align: center; font-size: 12px; '>Current Favorite Movie:  </p>", unsafe_allow_html=True)
-                        
-						
-                        with col3:
-                            st.image('resources/imgs/w.png')
-                            st.write("<p style='text-align: center; font-size: 20px; font-family: cursive; '>Matlala Nyama</p>", unsafe_allow_html=True)
-                            st.write("<p style='text-align: center; font-size: 12px; '>Occupation: </p>", unsafe_allow_html=True)
-                            st.write("<p style='text-align: center; font-size: 12px; '>Current Favorite Movie:  </p>", unsafe_allow_html=True)
-                            st.write("")
-                            st.write("")
-
-                with st.container():
-                    st.write("<p style='text-align: center; font-size: 50px; '>Project Management Team</p>", unsafe_allow_html=True)
-                    
-                    with st.container():
-                    
-                        col1, col2, col3 = st.columns(3)
-				
-                with col2:
-                    st.image('resources/imgs/W.png') 
-                    st.write("<p style='text-align: center; font-size: 20px; font-family: cursive; '>Temishka Surname</p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Occupation: </p>", unsafe_allow_html=True)
-                    st.write("<p style='text-align: center; font-size: 12px; '>Current Favorite Movie:  </p>", unsafe_allow_html=True)           
-
-
-            with col13:
-                st.title("Contact Details")
-                st.write("Office Line : (+27) 43 656 9800")
-                st.write("Emergency Line : (+27) 43 656 9000")
-                st.write("Email Address : info@bitsandbytes.co.za")
-                st.image("resources/imgs/Logo.jpg", width=300)
-
-    if page_selection == "FlixWhiz Info":
-        st.write("")
-
-
-
-
-
-
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
@@ -654,7 +401,7 @@ if page_selection == "FlixWhiz Info":
     st.header("Why use our app?")
     st.write("With an ever-expanding library of movies across various genres, it can be overwhelming to choose what to watch next. That's where our Movie Recommender System comes to the rescue! By leveraging advanced machine learning algorithms, our app analyzes your movie-watching history, ratings, and preferences to curate a list of recommendations that align perfectly with your interests.")
 
-    st.header("How it works")
+    st.header("How it works?")
     st.write("Our app employs a powerful technique known as collaborative filtering. It works in two primary ways:")
     st.write("")
     st.write("1. User-Based Collaborative Filtering: This method identifies users with similar movie tastes to yours. It then recommends movies that those like-minded users have enjoyed but you haven't watched yet. By connecting you with individuals who share your cinematic inclinations, this approach ensures that you'll discover hidden gems that are likely to resonate with you.")
@@ -668,8 +415,30 @@ if page_selection == "FlixWhiz Info":
 
 
 if page_selection == "Analysis":
-        st.header("Exploratory data analysis")
-    
+        st.title("Exploratory data analysis")
+        st.header('Discovering the most viewed movie genres')
+        st.write('In the graph below, we explore the movie dataset to gain insights into the most viewed movie genres. '
+         'By analyzing the most popular genres, we aim to understand the audience\'s preferences and trends in the movie industry.')
+        st.image('resources/imgs/genres.png')
+        st.write('The graph reveals that "Drama" is the most prevalent genre, followed by "Comedy" and "Thriller." '
+                 "Users can quickly grasp the popularity of different movie genres within the app's community, encouraging them to explore movies in those genres.")
+        st.write('The dataset provides valuable insights into the distribution of movie genres, which can inform future movie recommendations and content curation.')
+
+        st.header("Exploring the top 15 most popular movies by ratings")
+        st.write('By analyzing the highest-rated movies, we aim to identify the audience\'s favorite films and understand what contributes to their popularity.')
+        st.image("resources/imgs/top15.png")
+        st.write('The graph highlights the movies that have resonated the most with the audience, receiving exceptional ratings from users, with "SHawshank Redemption" and "Forest Gump" topping the chart. '
+         "Recommending popular movies with high ratings increases the likelihood of user satisfaction.")
+        st.write(" Users are more likely to enjoy movies that have received positive acclaim, leading to a positive experience with the app."
+         'These top-rated films can provide valuable insights into the features and elements that contribute to a movie\'s success and appeal.')   
+
+        st.header("Unveiling the Top 5 Directors with the Most Movies")
+        st.write('Below we explore the movie dataset to identify the top 5 directors who have directed the most movies. '
+         'By analyzing the prolific directors, we aim to recognize the filmmakers who have made a significant impact on the movie industry.')
+        st.image("resources/imgs/direct.png")
+        st.write('This chart sheds light on the directors who have made a considerable impact on the movie world. '
+         'By understanding the breadth and depth of their filmography, we can gain insights into the variety of genres and styles they have explored.'
+         "Incorporating data on top directors can influence the app's recommendations. If a user enjoys a movie from a specific director, the recommender system can suggest other movies directed by the same filmmaker.")
 
 if __name__ == '__main__':
-      main()
+    main()
