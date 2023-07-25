85f29e4dd3dd3876b4ef981b2bf0de3a07e38375
Fumani09 <127149453+Fumani09@users.noreply.github.com>
2023-07-26 01:50:44 +0200

    Add files via upload

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -31,6 +31,9 @@ import streamlit as st
 # Data handling dependencies
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
@@ -39,35 +42,68 @@ from recommenders.content_based import content_model
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
+movies_df = pd.read_csv('resources/data/movies.csv')
+train_df = pd.read_csv('resources/data/train.csv')
+
+#Let's build the function which are to be used in the home tab
+
+def ratings_distplot(df, column='rating'):
+    plt.figure(figsize=(15, 10))
+    ax = sns.distplot(df[column], bins=10, kde=False, hist_kws=dict(alpha=0.6), color="green")
+    mean = df[column].mean()
+    median = df[column].median()
+    plt.axvline(x=mean, label=f'mean {round(mean, 2)}', color='green', lw=3, ls='--')
+    plt.axvline(x=median, label=f'median {median}', color='#4DA017', lw=3, ls='--')
+    plt.xlim((0.5, 5))
+    plt.ylim((0, 250000))
+    plt.title('Distribution of Ratings', fontsize=14)
+    plt.xlabel('Rating')
+    plt.ylabel('Frequency')
+    plt.legend()
+    return plt.show()
+
+# Initialize a session state variable that tracks the sidebar state (either 'expanded' or 'collapsed').
+if 'sidebar_state' not in st.session_state:
+    st.session_state.sidebar_state = 'expanded'
+
+st.set_page_config(page_title="FSC_TECH", page_icon="resources/imgs/4.png",
+                   initial_sidebar_state=st.session_state.sidebar_state, layout="centered")
 
 # App declaration
 def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
+    c1, c2 = st.columns([1, 2])
+    with c1:
+
+        st.image(
+            "resources/imgs/logo.png",
+            width=200,)
+    with c2:
+        st.header("Movie Recommender System")
     
-    page_options = ["Home","Recommender System","Solution Overview", 'About us', "FAQ"]
+    row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
+    with row0_1:
+        st.subheader('')
+    row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
+    
+    #page_options = ["Home","Recommender System","Solution Overview", 'About us', "FAQ"]
+    HomeTab, RecommenderSystemTab, SolutionOverviewTab, AboutUsTab, FAQTab = st.tabs(["Home","Recommender System","Solution Overview", 'About us', "FAQ"])
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
-    st.sidebar.image('resources/imgs/4.png',use_column_width=False)
-    st.sidebar.subheader("Movie Recommender Engine 💡")
-    page_selection = st.sidebar.selectbox("Choose Option", page_options)
-    if page_selection == "Recommender System":
+    #st.sidebar.image('resources/imgs/4.png',use_column_width=False)
+    #st.sidebar.subheader(" 💡")
+    #page_selection = st.sidebar.selectbox("Choose Option", page_options)
+    with RecommenderSystemTab:
         # Header contents
-        c1, c2 = st.columns([1, 2])
-        with c1:
 
-            st.image(
-                "resources/imgs/logo.png",
-                width=250,)
-        with c2:
-            st.title("Movie Recommender Engine 💡")
         # We need to set up session state via st.session_state so that app interactions don't reset the app.
         if not "valid_inputs_received" in st.session_state:
             st.session_state["valid_inputs_received"] = False
-        st.write('# ')
+        st.write('# Movie Recommender Engine')
         st.write("### let's Get You started! Get Your Recommendations Below")
         st.image('resources/imgs/Image_header.png',use_column_width=True)
         
@@ -113,13 +149,12 @@ def main():
 
 
     # -------------------------------------------------------------------
-
+######################################################### Solution Overview ###########################################################
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    if page_selection == "Solution Overview":
+    with SolutionOverviewTab:
         st.title("Solution Overview")
-        st.write("We the FSC_TECH company data science company offers solutions that leverage data analytics, machine learning,")
-        st.write(" and artificial intelligence to solve complex problems, also recommend and provide valuable insights to clients. ")
-        st.write("Here's a detailed overview of the various components and processes involved in the operations of FSC_TECH company:")
+        st.write("We the FSC_TECH company data science company offers solutions that leverage data analytics, machine learning, and artificial intelligence to solve complex problems, also recommend and provide valuable insights to clients. ")
+        st.write("Here are detailed overview of the various components and processes involved in the operations of FSC_TECH company:")
         st.write("A: Problem Identification and Consultation")
         st.write("B: Data Acquisition and Integration")
         st.write("C: Exploratory Data Analysis (EDA)")
@@ -130,27 +165,77 @@ def main():
         st.write("H: Continuous Monitoring and Maintenance")
         st.write("I: Reporting and Visualization")
         st.write("J: Ongoing Support and Collaboration")
+        
+        
+#################################################### Home Page #########################################################tr
+   
+    with HomeTab:
+        st.header(
+                "FSC_Tech Home Page")
+        st.markdown('Hello There👋👋')
+        st.markdown("FSC_TECH company are into building recommendation system for companies and also offers the best services for our clients. With the power of Machine Learning, we're set to provide movie recommendations on recommender system like never before. Join us on our Streamlit dashboard and let the data-driven exploration begin!🥳🥳🥳")
+        st.subheader("Raw Data")
+        with st.expander("Click here to view the raw data"):
+            st.dataframe(movies_df)
+        
+        column_1, column_2 = st.columns([2, 3])
 
-    if page_selection == "Home":
-        st.image('resources/imgs/logo.png',use_column_width=True)
-        st.title("Home")
-        st.write("FSC_TECH company are into building recommendation system for companies and also offers the best services for our clients.")
-        
-#         with st.expander("Click here to view the raw data"):
-#             st.dataframe(filtered_df)
-        
-        
-    # Building out the About Us page
-    if page_selection == "About us":
-        c1, c2 = st.columns([1, 2])
-        with c1:
+        with column_1:
             st.image(
                 "resources/imgs/4.png",
-                width=150,)
+                width=60,
+            )
+            st.subheader("Movies Per Rating")
+            st.write("This graph shows the relationship between the movies and the rating of each movie in the dataset. It is a strong indicator on to use and check distribution of the ratings")
+        with column_2:
+            st.image(
+                "resources/imgs/bar.png",
+                width=350,
+            )         
             
+        st.header(" ")
+        st.markdown("---")
+        column_1, column_2 = st.columns([2, 3])
+
+        with column_1:
             
-        with c2:
-            st.title(
+            st.image(
+                "resources/imgs/4.png",
+                width=60,
+            )
+            st.subheader("Movies Per Rating")
+         
+            st.write("This graph shows the relationship between the movies and the rating of each movie in the dataset. It is a strong indicator on to use and check distribution of the ratings")
+        with column_2:
+            #create a set of english stopwords
+            from nltk.corpus import stopwords
+            stopwords = set(stopwords.words('english'))
+            #create a list of all the words in the 'title' column
+            words = ' '.join(movies_df['title'])
+            #create a wordcloud object
+            wordcloud = WordCloud(width = 500, height = 400, 
+                            background_color ='white', 
+                            stopwords = stopwords, 
+                            min_font_size = 10).generate(words)
+
+            # plot the wordcloud
+            plt.figure(figsize = (10, 6), facecolor = None) 
+            plt.imshow(wordcloud) 
+            plt.axis("off") 
+            plt.tight_layout(pad = 0) 
+            plt.show() 
+            
+        st.markdown("---")
+        st.header(" ")
+    # We need to set up session state via st.session_state so that app interactions don't reset the app.
+    if not "valid_inputs_received" in st.session_state:
+        st.session_state["valid_inputs_received"] = False
+        
+        
+###################################### Building out the About Us page ####################################################
+
+    with AboutUsTab:
+        st.title(
                 "FSC_TECH company")
 
         st.subheader("Our Vision:")
@@ -188,22 +273,15 @@ def main():
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
     
-    # Building out the FAQ page
-    if page_selection == "FAQ":
-        c1, c2 = st.columns([1, 2])
-        with c1:
-
-            st.image(
-                "resources/imgs/logo.png",
-                width=200,)
-        with c2:
-            st.title("Frequently Asked Questions")
+############################################## Building out the FAQ page ############################################################
+    with FAQTab:
+        st.title("Frequently Asked Questions")
         st.markdown("Welcome to the FAQ page. Here are some frequently asked questions:")
         st.write("")
         
         faq_data = [("What is Streamlit?", "Streamlit is an open-source Python library that makes it easy to create web applications for data science and machine learning projects."),
                    ("How do I install Streamlit?", "You can install Streamlit using pip: `pip install streamlit`."),
-                    ("Q1: What is a movie recommender system?","A movie recommender system is a type of software or algorithm that suggests movies to users based on their preferences and viewing history. It analyzes user data, such as movie ratings, viewing habits, and interactions, to make personalized movie recommendations."), 
+                    ("What is a movie recommender system?","A movie recommender system is a type of software or algorithm that suggests movies to users based on their preferences and viewing history. It analyzes user data, such as movie ratings, viewing habits, and interactions, to make personalized movie recommendations."), 
                     ("How do movie recommender systems work?","Movie recommender systems typically use two main approaches: collaborative filtering and content-based filtering. Collaborative filtering analyzes user behavior and identifies users with similar preferences to generate recommendations. Content-based filtering focuses on the attributes of movies themselves and suggests items with similar characteristics to those the user has liked in the past."),
                     ("What are the benefits of using a movie recommender system?","Movie recommender systems provide several benefits. They help users discover new movies that match their interests, leading to enhanced user satisfaction and engagement. For movie platforms, recommender systems can increase user retention, improve movie ratings, and drive revenue through increased movie consumption."),
                     ("What are hybrid recommender systems?","Hybrid recommender systems combine multiple recommendation techniques to improve the quality of recommendations. For example, a hybrid system might merge collaborative filtering and content-based filtering to provide more accurate and diverse movie suggestions."), 
@@ -218,8 +296,7 @@ def main():
                 st.markdown(f"**Q{idx+1}: {question}**")
                 st.write(answer)
                 st.markdown("---")
-
-            # Example FAQ data (question, answer) pairs
+############################################################################################################################################
 
 
 if __name__ == '__main__':
