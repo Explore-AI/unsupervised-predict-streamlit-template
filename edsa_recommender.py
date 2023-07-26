e3baa5afe9deb5e9cfddc5022dc9a01c15c33041
DevonWoodman <dwoodman.dw@gmail.com>
2023-07-26 10:56:10 +0200

    Collab Filter

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -118,30 +118,30 @@ def main():
         # Perform top-10 movie recommendation generation
         if sys == 'Content Based Filtering':
             if st.button("Recommend"):
-                #try:
-                with st.spinner('Crunching the numbers...'):
-                    top_recommendations = content_model(movie_list=fav_movies,
-                                                        top_n=10)
-                st.title("We think you'll like:")
-                for i,j in enumerate(top_recommendations):
-                    st.subheader(str(i+1)+'. '+j)
-                #except:
-                #    st.error("Oops! Looks like this algorithm does't work.\
-                #              We'll need to fix it!")
-
-
-        if sys == 'Collaborative Based Filtering':
-            if st.button("Recommend"):
                 try:
                     with st.spinner('Crunching the numbers...'):
-                        top_recommendations = collab_model(movie_list=fav_movies,
-                                                           top_n=10)
+                        top_recommendations = content_model(movie_list=fav_movies,
+                                                            top_n=10)
                     st.title("We think you'll like:")
                     for i,j in enumerate(top_recommendations):
                         st.subheader(str(i+1)+'. '+j)
                 except:
                     st.error("Oops! Looks like this algorithm does't work.\
                               We'll need to fix it!")
+
+
+        if sys == 'Collaborative Based Filtering':
+            if st.button("Recommend"):
+                #try:
+                with st.spinner('Crunching the numbers...'):
+                    top_recommendations = collab_model(movie_list=fav_movies,
+                                                       top_n=10)
+                st.title("We think you'll like:")
+                for i,j in enumerate(top_recommendations):
+                    st.subheader(str(i+1)+'. '+j)
+                #except:
+                #    st.error("Oops! Looks like this algorithm does't work.\
+                #              We'll need to fix it!")
 
 
     # -------------------------------------------------------------------
