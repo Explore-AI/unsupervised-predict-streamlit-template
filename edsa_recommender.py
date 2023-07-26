b2b8ab8d46ea13dfae083156810aa36ebdd632ee
xichavolimpia <xichavolimpia@gmail.com>
2023-07-26 13:33:16 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -28,7 +28,7 @@
 # Streamlit dependencies
 import streamlit as st
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 import joblib,os
 import csv
 from PIL import Image, ImageDraw
